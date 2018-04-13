22b1af9a1b32c2ccb7332ae44604db5d2d771d1b
cohyou <cohyou@ironoir.io>
2018-04-13 17:00:00 +0900

    move sql files

--- kskp/sql/schema.sql
+++ kskp/sql/schema.sql
@@ -4,16 +4,20 @@ CREATE TABLE users (
     password CHAR(64) NOT NULL,
     name VARCHAR(64) NOT NULL DEFAULT '',
     created_at TIMESTAMP DEFAULT (DATETIME('now','localtime')),
-    modified_at TIMESTAMP DEFAULT (DATETIME('now','localtime'))
+    modified_at TIMESTAMP DEFAULT (DATETIME('now','localtime')),
+    creator VARCHAR(256) NOT NULL DEFAULT '',
+    modifier VARCHAR(256) NOT NULL DEFAULT ''
 );
 
 CREATE TABLE projects (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     uuid CHAR(36) NOT NULL DEFAULT '',
     name VARCHAR(256) NOT NULL DEFAULT '',
-    author INTEGER NOT NULL DEFAULT 0,
+    creator_name INTEGER NOT NULL DEFAULT 0,
     created_at TIMESTAMP DEFAULT (DATETIME('now','localtime')),
-    modified_at TIMESTAMP DEFAULT (DATETIME('now','localtime'))
+    modified_at TIMESTAMP DEFAULT (DATETIME('now','localtime')),
+    creator VARCHAR(256) NOT NULL DEFAULT '',
+    modifier VARCHAR(256) NOT NULL DEFAULT ''
 );
 
 CREATE TABLE users_x_projects (
