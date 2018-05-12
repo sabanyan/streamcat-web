cfa3b3128f5969744823ff124b0fb27b1f7c97a4
Takahiro Izuchi <izuchi@impath.jp>
2018-05-12 12:58:34 +0900

    Revert

--- src/js/model/OperatorModel.js
+++ src/js/model/OperatorModel.js
@@ -1,65 +1,29 @@
-// @flow
 import ModelUtil from '../utils/ModelUtil'
 
-type TOperatorModel = {
-  id:string;
-  operator:{};
-  position:{
-    x:number,
-    y:number
-  };
-  size:{
-    width:number,
-    height:number
-  };
-  text:string;
-  property:{};
-  parameters:{};
-}
+export default class OperatorModel {
 
-export default class OperatorModel<TOperatorModel> {
-  id:string = ModelUtil.getId();
-  operator:{};
-  position:{
-    x:number,
-    y:number
-  } = {
-    x: 0,
-    y: 0
-  };
-  size:{
-    width:number,
-    height:number
-  };
-  text:string;
-  property:{};
-  parameters:{};
-
-  constructor (model:TOperatorModel) {
-    this.id = model.id
-    this.operator = model.operator
-    this.position = model.position
-    this.size = model.size
-    this.text = model.text
-    this.property = model.property
-    this.parameters = model.parameters
+  constructor ({id = ModelUtil.getId(), operator = null,position={x:0,y:0},size={width:0,height:0}, width = null, height = null, text = "",property = {},parameters = {} } = {}) {
+    this.id = id
+    this.operator = operator
+    this.position = position
+    this.size = size
+    this.text = text
+    this.property = property
+    this.parameters = parameters
   }
 
-  setPosition (x:number, y:number) {
-    console.log(this.position)
+  setPosition (x , y) {
     this.position.x = x
     this.position.y = y
   }
 
-  setSize (width:number, height:number) {
+  setSize (width, height) {
     this.size.width = width
     this.size.height = height
   }
 
-  setFrame (x:number, y:number, width:number, height:number) {
+  setFrame (x, y, width, height) {
     this.setPosition(x, y)
     this.setSize(width, height)
   }
 }
-
-const a = new OperatorModel(model);
