d91ec05692591689d7295805705418e7e36e80a5
izuchy <>
2018-06-09 10:34:51 +0900

    Add preview modal and standard modal

---
+++ src/js/components/shared/Modal/Preview/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import classnames from "classnames";
+
+export default class PreviewModal extends React.Component{
+
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+
+        const {id, children,close_button, visible, title, footer} = this.props
+        const modal_class = classnames("modal fade preview right", {
+            "show in": visible,
+            "none-pointer-events": !visible,
+        })
+
+        return <div className={modal_class} style={{display: "block"}} id={id}>
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">{title}</h5>
+                        {close_button}
+                    </div>
+                    <div className="modal-body">
+                        <div>{children}</div>
+                    </div>
+                    {footer}
+                </div>
+            </div>
+        </div>
+    }
+
+}
--- src/js/components/shared/Modal/index.jsx
+++ src/js/components/shared/Modal/index.jsx
@@ -1,185 +1,187 @@
 // @flow
 import * as React from 'react'
 import Constants from '../../../constants/index'
+import StandardModal from "./Standard";
+import PreviewModal from "./Preview";
 
 type Props = {
-  id: string,
-  title?: string,
-  content?: React.Node,
-  dynamic?: boolean,
-  preview?: boolean,
-  footer?: boolean,
-  modal?: boolean,
-  ok?: boolean,
-  cancel?: string,
-  close?: boolean,
-  done?: string,
-  children?: React.Node
+    id: string,
+    title?: string,
+    content?: React.Node,
+    dynamic?: boolean,
+    preview?: boolean,
+    footer?: boolean,
+    modal?: boolean,
+    ok?: boolean,
+    cancel?: string,
+    close?: boolean,
+    done?: string,
+    children?: React.Node
 }
 
 type State = {
-  visible: boolean,
-  content?: any,
-  title?: string
+    visible: boolean,
+    content?: any,
+    title?: string
 }
 
-export default class Modal extends React.Component<Props,State> {
-  static defaultProps = {
-    ok: false,
-    done: "確定",
-    cancel: "キャンセル",
-    close: true,
-    preview: false,
-    footer: true,
-    modal: false,
-  }
+export default class Modal extends React.Component<Props, State> {
+    static defaultProps = {
+        ok: false,
+        done: "確定",
+        cancel: "キャンセル",
+        close: true,
+        preview: false,
+        footer: true,
+        modal: false,
+    }
 
-  constructor (props:Props) {
-    super(props)
-    this.state= {visible: false,content:null,title: this.props.title}
-  }
+    constructor(props: Props) {
+        super(props)
+        this.state = {visible: false, content: null, title: this.props.title}
+    }
 
 
-  componentWillMount () {
-    const self = this
-    /**
-     * モーダルのリスナー処理
-     */
-    window.emitter.removeListener(Constants.event.MODAL_EVENT + this.props.id)
-    window.emitter.addListener(Constants.event.MODAL_EVENT + this.props.id, (context) => {
-      if (context.visible !== undefined) {
-        self.setState({
-          visible: context.visible
+    componentWillMount() {
+        const self = this
+        /**
+         * モーダルのリスナー処理
+         */
+        window.emitter.removeListener(Constants.event.MODAL_EVENT + this.props.id)
+        window.emitter.addListener(Constants.event.MODAL_EVENT + this.props.id, (context) => {
+            if (context.visible !== undefined) {
+                self.setState({
+                    visible: context.visible
+                })
+            }
+            if (context.content !== undefined) {
+                self.setState({
+                    content: context.content
+                })
+            }
+            if (context.title !== undefined) {
+                self.setState({
+                    title: context.title
+                })
+            }
         })
-      }
-      if (context.content !== undefined){
-        self.setState({
-          content: context.content
+
+    }
+
+    /**
+     * OKボタンが押された（props.ok が trueの場合のみ有効）
+     */
+    onClickOK() {
+        window.emitter.emit(Constants.event.MODAL_ON_CLICK_OK + this.props.id, {id: this.props.id})
+        this.setState({
+            visible: false
         })
-      }
-      if (context.title !== undefined){
-        self.setState({
-          title: context.title
+    }
+
+    /**
+     * 確定ボタンが押された
+     */
+    onClickDone() {
+        window.emitter.emit(Constants.event.MODAL_ON_CLICK_DONE + this.props.id, {id: this.props.id})
+    }
+
+    /**
+     * キャンセルボタンが押された
+     */
+    onClickCancel() {
+        window.emitter.emit(Constants.event.MODAL_ON_CLICK_CANCEL + this.props.id, {id: this.props.id})
+        this.setState({
+            visible: false
         })
-      }
-    })
-
-  }
-
-  /**
-   * OKボタンが押された（props.ok が trueの場合のみ有効）
-   */
-  onClickOK () {
-    window.emitter.emit(Constants.event.MODAL_ON_CLICK_OK + this.props.id, {id: this.props.id})
-    this.setState({
-      visible: false
-    })
-  }
-
-  /**
-   * 確定ボタンが押された
-   */
-  onClickDone () {
-    window.emitter.emit(Constants.event.MODAL_ON_CLICK_DONE + this.props.id, {id: this.props.id})
-  }
-
-  /**
-   * キャンセルボタンが押された
-   */
-  onClickCancel () {
-    window.emitter.emit(Constants.event.MODAL_ON_CLICK_CANCEL + this.props.id, {id: this.props.id})
-    this.setState({
-      visible: false
-    })
-  }
-
-  /**
-   * 背景がクリックされた
-   */
-  onClickBackdrop(){
-    if(!this.props.modal){
-      this.setState({visible: false})
-    }
-  }
-
-  render () {
-
-    const visible = this.state.visible
-    const {preview} = this.props
-
-    /**
-     * 背景
-     */
-    const backdrop = (visible) ? <div onClick={() => this.onClickBackdrop()} className={"modal-backdrop fade show" + ((preview) ? " preview" : "")} /> :
-      <div className="modal-backdrop fade" style={{pointerEvents: "none"}} />
-
-    let buttons
-
-    /**
-     * OKボタン
-     */
-    if (this.props.ok) {
-      buttons = <button type="button" className="btn btn-link" data-dismiss="modal"
-                        onClick={() => this.onClickOK()}>
-        OK
-      </button>
     }
 
     /**
-     * 確定・キャンセルボタン
+     * 背景がクリックされた
      */
-    if (!this.props.ok) {
-      buttons = <div>
-        <button type="button" className="btn btn-link" onClick={() => this.onClickCancel()}>
-          {this.props.cancel}
-        </button>
-        &nbsp;
-        <button type="button" className="btn btn-link" onClick={() => this.onClickDone()}>{this.props.done}</button>
-      </div>
+    onClickBackdrop() {
+        if (!this.props.modal) {
+            this.setState({visible: false})
+        }
     }
 
-    let close
+    render() {
 
-    /**
-     * 閉じるボタン
-     */
-    if (this.props.close) {
-      close =
-        <button type="button" className="close" onClick={() => {this.setState({visible: false})}}>
-          <span>&times;</span>
-        </button>
-    }
+        const {visible, title, content} = this.state
+        const {preview, ok, close, footer, done, cancel, children} = this.props
 
-    if(this.props.preview){
-      buttons = null
-    }
+        /**
+         * 背景
+         */
+        const backdrop = (visible) ? <div onClick={() => this.onClickBackdrop()}
+                                          className={"modal-backdrop fade show" + ((preview) ? " preview" : "")}/> :
+            <div className="modal-backdrop fade" style={{pointerEvents: "none"}}/>
 
-    let footer
-    if(this.props.footer){
-      footer = <div className="modal-footer">
-          {buttons}
-        </div>
-    }
+        let buttons
 
-    return (
-      <div key={this.props.id}>
-        <div className={"modal fade" + ((visible) ? " show in" : " none-pointer-events") + ((preview) ? " preview right" : "")} style={{display: "block"}}
-             id={this.props.id}>
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">{this.state.title}</h5>
-                {close}
-              </div>
-              <div className="modal-body">
-                <div>{(this.props.dynamic)?this.state.content:this.props.children}</div>
-              </div>
-              {footer}
+        /**
+         * OKボタン
+         */
+        if (ok) {
+            buttons = <button type="button" className="btn btn-link" data-dismiss="modal"
+                              onClick={() => this.onClickOK()}>
+                OK
+            </button>
+        }
+
+        /**
+         * 確定・キャンセルボタン
+         */
+        if (!ok) {
+            buttons = <div>
+                <button type="button" className="btn btn-link" onClick={() => this.onClickCancel()}>
+                    {cancel}
+                </button>
+                &nbsp;
+                <button type="button" className="btn btn-link" onClick={() => this.onClickDone()}>{done}</button>
             </div>
-          </div>
+        }
+
+        /**
+         * 閉じるボタン
+         */
+        let close_button
+        if (close) {
+            close_button =
+                <button type="button" className="close" onClick={() => {
+                    this.setState({visible: false})
+                }}>
+                    <span>&times;</span>
+                </button>
+        }
+
+        if (preview) {
+            buttons = null
+        }
+
+        let modal_footer
+        if (footer) {
+            modal_footer = <div className="modal-footer">
+                {buttons}
+            </div>
+        }
+
+        const {id, dynamic} = this.props
+
+        let modal
+        let modal_body = (dynamic) ? content : children
+        if (preview) {
+            modal = <PreviewModal id={id} title={title} footer={modal_footer} close_button={close_button} visible={visible}>
+                {modal_body}
+            </PreviewModal>
+        } else {
+            modal = <StandardModal id={id} title={title} footer={modal_footer} close_button={close_button} visible={visible}>
+                {modal_body}
+            </StandardModal>
+        }
+
+        return <div>
+            {modal}
+            {backdrop}
         </div>
-        {backdrop}
-      </div>
-    )
-  }
+    }
 }
