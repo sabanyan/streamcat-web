40ae6260c146697f04046fe52387a5b8d504acb8
Takahiro Izuchi <izuchi@impath.jp>
2018-06-09 17:45:47 +0900

    Update Multi inspector

--- src/js/components/FlowEditorContainer/Inspector/MultiInspector/index.jsx
+++ src/js/components/FlowEditorContainer/Inspector/MultiInspector/index.jsx
@@ -6,14 +6,29 @@ import ModalUtil from '../../../../utils/ModalUtil'
 import DataTable from '../../../shared/DataTable/index'
 import OperatorModel from '../../../../model/OperatorModel'
 import Inspector from '../Inspector'
-import type {FlowEditorProps} from "../../index";
+import type { FlowEditorProps } from '../../index'
+import style from '../style.scss'
 
 class MultiInspector extends React.Component<FlowEditorProps> {
-    render() {
-        return <Inspector header={"複数選択"} title={"プロパティ"}>
-               複数選択されています
-        </Inspector>
+  onClickDelete (e: Event) {
+    if (window.confirm('これらオペレーターを削除しますか？')) {
+      let {selected_step_ids} = this.props
+      this.props.deleteSteps(selected_step_ids)
+      this.props.selectSteps()
     }
+  }
+
+  render () {
+    return <Inspector header={this.props.selected_step_ids.length + ' files'} title="">
+      <div className={style.hr} />
+      <div className="kskp-form">
+        <div className="btn btn-danger btn-block py-8px text-14px"
+             onClick={(e) => this.onClickDelete(e)}>
+          削除する
+        </div>
+      </div>
+    </Inspector>
+  }
 
 }
 
