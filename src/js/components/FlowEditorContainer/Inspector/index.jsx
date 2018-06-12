b3d9d111087f391d3bd77ca2437ddaa14d2f3f32
Takahiro Izuchi <izuchi@impath.jp>
2018-06-12 13:46:36 +0900

    Update inspector

--- src/js/components/FlowEditorContainer/Inspector/index.jsx
+++ src/js/components/FlowEditorContainer/Inspector/index.jsx
@@ -7,29 +7,31 @@ import OperatorModel from '../../../model/OperatorModel'
 import classnames from 'classnames'
 import style from './style.scss'
 import type { FlowEditorProps } from '../index'
-import MultiInspector from "./MultiInspector";
+import MultiInspector from './MultiInspector'
 
 class Inspector extends React.Component<FlowEditorProps> {
 
   render () {
     let {selected_step_ids} = this.props
 
-    let property
+    let property,show
     const selected_step = this.props.steps[selected_step_ids[0]]
 
-      if(selected_step_ids.length == 1){
-          if (selected_step instanceof DataSourceModel) {
-              property = <DataSourceInspector {...this.props}></DataSourceInspector>
-          } else if (selected_step instanceof OperatorModel) {
-              property = <OperatorInspector {...this.props}></OperatorInspector>
-          }
-      }else if(!selected_step_ids.length){
-        return null
-      }else{
-          property = <MultiInspector {...this.props}></MultiInspector>
+    if (selected_step_ids.length == 1) {
+      if (selected_step instanceof DataSourceModel) {
+        property = <DataSourceInspector {...this.props}></DataSourceInspector>
+      } else if (selected_step instanceof OperatorModel) {
+        property = <OperatorInspector {...this.props}></OperatorInspector>
       }
+      show = true
+    } else if (!selected_step_ids.length) {
+      show = false
+    } else {
+      property = <MultiInspector {...this.props}></MultiInspector>
+      show = true
+    }
 
-    const property_class = classnames(style.kskp_property,{ [style.in]:true})
+    const property_class = classnames(style.property, {[style.in]: show})
 
     return <div className={property_class}>
       {property}
--- src/js/components/FlowEditorContainer/ToolBar/ToolBarButton/index.jsx
+++ src/js/components/FlowEditorContainer/ToolBar/ToolBarButton/index.jsx
@@ -4,11 +4,10 @@ import classnames from 'classnames'
 
 const ToolBarButton = (props) => {
   const {onClick,children,disabled,icon} = props;
-  const icon_class = classnames("material-icons",[style.icon],{[style.disabled]:disabled})
-  const text_class = classnames([style.text],{[style.disabled]:disabled})
+  const icon_class = classnames("material-icons",[style.icon])
   return <button type="button" className={style.toolbar_btn} disabled={disabled} onClick={onClick}>
     <i className={icon_class} dangerouslySetInnerHTML={{__html:icon}}></i>
-    <div className={text_class}>
+    <div className={style.text}>
       {children}
     </div>
   </button>
