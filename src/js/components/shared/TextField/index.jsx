c8e243e3fa0cca67fe0e243dc21cb8ebde83a587
Takahiro Izuchi <izuchi@impath.jp>
2018-06-21 08:31:15 +0900

    Add textfield with validation

---
+++ src/js/components/shared/TextField/index.jsx
@@ -0,0 +1,90 @@
+// @flow
+import React from 'react'
+import classnames from 'classnames';
+import style from './style.scss'
+
+type Props={
+  placeholder:string;
+  onChange: Function;
+  rules: {}
+}
+
+export default class TextField extends React.Component<Props>{
+  static defaultProps = {
+    rules: null
+  }
+
+  constructor(props){
+    super(props)
+    this.state = {
+      validation: true,
+      validation_messages: []
+    }
+  }
+
+  validateField(e,rules){
+    let validation_messages = []
+    Object.keys(rules).forEach((rule)=>{
+      const value = rules[rule]
+      switch (rule){
+        case "required":
+          if(value === true){
+            if(e.target.value === ""){
+              validation_messages.push("入力されていません")
+            }
+          }
+          break
+        case "minlength":
+          if(e.target.value.length < value){
+            validation_messages.push("最低" + value + "文字の入力が必要です")
+          }
+          break
+        case "maxlength":
+          if(e.target.value.length < value){
+            validation_messages.push("最大" + value + "文字までです")
+          }
+          break
+      }
+    })
+    return validation_messages
+  }
+
+  onChange(e){
+    const {onChange,rules} = this.props
+    let validation_messages = this.validateField(e,rules)
+    const validation = {
+      validation: (!validation_messages.length),
+      validation_messages: validation_messages
+    }
+    onChange(e,validation)
+    this.setState(validation)
+  }
+
+  renderValidationMessage(){
+    const {validation_messages} = this.state
+    const messages = validation_messages.map((message,index)=>{
+      return <li key={index} className={style.message}>{message}</li>
+    })
+    return <div>
+      <ul className={style.validation_messages}>
+        {messages}
+      </ul>
+    </div>
+
+  }
+
+  render(){
+    const {placeholder} = this.props
+    const {validation} = this.state
+
+    const input_class = classnames("form-control",{
+      [style.error]: !validation
+    })
+
+    return <div>
+      <input type="text" ref={"input"} className={input_class} placeholder={placeholder} onChange={(e)=>this.onChange(e)}></input>
+      {this.renderValidationMessage()}
+    </div>
+  }
+
+}
