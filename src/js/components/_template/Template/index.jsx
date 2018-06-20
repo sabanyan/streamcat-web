8814512f78b3da9e5fa639c2a084504e64ff46f4
Takahiro Izuchi <izuchi@impath.jp>
2018-06-21 08:30:03 +0900

    Add template

---
+++ src/js/components/_template/Template/index.jsx
@@ -0,0 +1,16 @@
+// @flow
+import React from 'react'
+import classnames from 'classnames';
+import style from './style.scss'
+
+export default class Template extends React.Component{
+
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return <div></div>
+  }
+
+}
