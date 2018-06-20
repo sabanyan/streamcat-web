6b74843525944dccfa09674145b91b28b38843f1
Takahiro Izuchi <izuchi@impath.jp>
2018-06-21 08:32:18 +0900

    Update http util

--- src/js/utils/HttpUtil.js
+++ src/js/utils/HttpUtil.js
@@ -1,7 +1,36 @@
-export default class HTTPUtil {
+import axios from 'axios'
+import Constants from '../constants'
 
-  static get(url){
+class HTTPUtil {
+  constructor () {
+    //default config
+    this.config = {}
+  }
 
+  mergeConfig (config) {
+    if (config) {
+      return Object.assign(this.config, config)
+    }
+  }
+
+  apiUrl (path) {
+    return '/api/v0/' + path
+  }
+
+  get (path, config) {
+    const merged_config = this.mergeConfig(config)
+    const url = this.apiUrl(path)
+    return axios.get(url, merged_config)
+  }
+
+  post (path, data, config) {
+    const merged_config = this.mergeConfig(config)
+    const url = this.apiUrl(path)
+    return axios.post(url, data, merged_config)
   }
 
 }
+
+//Singleton
+//ref:https://qiita.com/hkusu/items/d9ac2bd135e9e579e018
+export default new HTTPUtil()
