14f5a1d1371c4a0d89ac9e7f07bc60cf4afbb39b
yasuyuki okasaka <yasuyuki.okasaka@ksk-anl.com>
2018-06-29 16:06:19 +0900

    fix user_id

--- kskp/sql/schema.sql
+++ kskp/sql/schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE projects (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     uuid CHAR(36) NOT NULL DEFAULT '',
     name VARCHAR(256) NOT NULL DEFAULT '',
-    creator_name INTEGER NOT NULL DEFAULT 0,
+    creator_id INTEGER NOT NULL DEFAULT 0,
     created_at TIMESTAMP DEFAULT (DATETIME('now','localtime')),
     modified_at TIMESTAMP DEFAULT (DATETIME('now','localtime')),
     creator VARCHAR(256) NOT NULL DEFAULT '',
