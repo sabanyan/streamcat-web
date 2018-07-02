5b276ac7205777d38da483b620486eae729a2ecd
cohyou <cohyou@ironoir.io>
2018-07-02 16:16:57 +0900

    making sample by NI Japan...

--- kskp/engine/commands/mcmd/tablesplit.py
+++ kskp/engine/commands/mcmd/tablesplit.py
@@ -1,5 +1,12 @@
-class Mbucket:
-    pass
+from . import *
+
+class Mbucket(MCommand):
+    def __init__(self):
+        super().__init__()
+        self.name = 'mbucket'
+        self.description = '行分割'
+        self.parameters.append(Parameter('n', '行数'))
+        self.parameters.append(Parameter('f', '対象列名'))
 
 class Mmbucket:
     pass
