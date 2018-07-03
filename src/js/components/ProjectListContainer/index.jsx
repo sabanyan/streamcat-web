abc706357ecdf3feced24dcfa123382dbdd4162c
Takahiro Izuchi <izuchi@impath.jp>
2018-07-03 11:42:31 +0900

    Add margin

--- src/js/components/ProjectListContainer/index.jsx
+++ src/js/components/ProjectListContainer/index.jsx
@@ -159,7 +159,7 @@ export default class ProjectListContainer extends React.Component {
   }
 
   render () {
-    return <div className={'container'}>
+    return <div className={'container mt-40px'}>
       <Loader absolute={true} visible={this.state.is_loading}/>
       {this.renderAll()}
       <ModalManager/>
