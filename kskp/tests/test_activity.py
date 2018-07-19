845956771dfaed0905572fe1e05a170385a113dd
yasuyuki okasaka <yasuyuki.okasaka@ksk-anl.com>
2018-07-19 17:27:00 +0900

    fix make activity

--- kskp/tests/test_activity.py
+++ kskp/tests/test_activity.py
@@ -36,23 +36,24 @@ class ActivityTest(unittest.TestCase):
         user_name = 'ユーザ太郎'
         file_path = make_unfinished_histroy(flow_uuid, user_name)
 
-        # モックの作成
+        # jobの実行結果モックの作成
         frame_o_section = mock.MagicMock(uuid = 'section')
         frame_o_all = mock.MagicMock(uuid = 'all')
-        result = {'o_section':frame_o_section, 'o_all':frame_o_all}
+        job_lasts = {'o_section':frame_o_section, 'o_all':frame_o_all}
 
         # 更新前の中身をテスト
         before_execute_data = json.loads(file_path.read_text(encoding='utf-8'))
         self.assertEqual([data for data in before_execute_data['data'].keys()], [])
 
         # 実行履歴ファイルの更新
-        make_finished_histroy(flow_uuid, file_path, result)
+        make_finished_histroy(flow_uuid, file_path, job_lasts)
 
         # 更新後の中身をテスト
         executed_data = json.loads(file_path.read_text(encoding='utf-8'))
-        self.assertEqual([data for data in executed_data['data'].keys()], [key for key in result.keys()])
+        self.assertEqual([data for data in executed_data['data'].keys()], [key for key in job_lasts.keys()])
         self.assertEqual(executed_data['data']['o_section']['uuid'], frame_o_section.uuid)
         self.assertEqual(executed_data['data']['o_all']['uuid'], frame_o_all.uuid)
+        self.assertEqual(executed_data['state'], '実行完了')
 
         # 後片付け
         file_path.unlink()
