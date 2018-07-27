ee147a37470b402790dcdb126253734e8a0f2af4
Takahiro Izuchi <izuchi@impath.jp>
2018-07-28 02:34:02 +0900

    fix warning

--- src/js/components/FlowEditorContainer/Paper/index.jsx
+++ src/js/components/FlowEditorContainer/Paper/index.jsx
@@ -9,16 +9,18 @@ import ZoomUtil from '../../../utils/ZoomUtil'
 class Paper extends React.Component<FlowEditorProps> {
   render () {
 
-    const {zoom,graph} = this.props
+    const {zoom, graph} = this.props
 
     const paperWidth = graph.width + Constants.paper.padding.right
     const paperHeight = graph.height + Constants.paper.padding.bottom
-    const viewWidth = ZoomUtil.zoomReverse(paperWidth,zoom)
-    const viewHeight = ZoomUtil.zoomReverse(paperHeight,zoom)
+    const viewWidth = ZoomUtil.zoomReverse(paperWidth, zoom)
+    const viewHeight = ZoomUtil.zoomReverse(paperHeight, zoom)
+
+    if (!paperWidth || !paperHeight) return null
 
     return <svg className={style.paper} width={paperWidth} height={paperHeight}
                 viewBox={'0 0 ' + viewWidth + ' ' + viewHeight}>
-      <Index/>
+      <Index />
       {this.props.children}
     </svg>
   }
