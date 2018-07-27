7b8ef30383633eeb53d77a3d7d3a3dda32aa1b2b
Takahiro Izuchi <izuchi@impath.jp>
2018-07-27 15:00:51 +0900

    Fix flow list

--- src/js/types/index.js
+++ src/js/types/index.js
@@ -1,13 +1,23 @@
-import DataFrameStepModel from './DataFrameStepModel'
-import CommandStepModel from './CommandStepModel'
-import SubFlowStepModel from './SubFlowStepModel'
+import CommandStepModel from '../model/Step/CommandStepModel'
+import SubFlowStepModel from '../model/Step/SubFlowStepModel'
+import DataFrameStepModel from '../model/Step/DataFrameStepModel'
+
 export type StepModelType = CommandStepModel|SubFlowStepModel|DataFrameStepModel
 export type CommandParamType = {
   label: string;
   name: string;
   type: string;
 }
+
 export type CommandPortType = {
   name: string;
   type: string;
+}
+
+export type FlowListDataType = {
+  label: string;
+  nodes: [StepModelType];
+  ports: [];
+  projectId: number;
+  uuid: string;
 }
