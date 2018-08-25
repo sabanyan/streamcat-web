dc37d239f4457c1e3d7228beef2587f184a9df5c
Takahiro Izuchi <izuchi@impath.jp>
2018-08-25 13:39:41 +0900

    show pdf link

--- src/js/utils/WebUtil.js
+++ src/js/utils/WebUtil.js
@@ -3,4 +3,8 @@ export default class WebUtil {
   static navigateURL(url:string){
     window.location.href = url
   }
+  static webURL(url:string):string{
+    url = url.replace("./","/")
+    return window.location.protocol + "//" +  window.location.host + url
+  }
 }
