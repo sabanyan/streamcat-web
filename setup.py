d231bc3bdd590fe051b00ba9cc5fb809e8dcc171
cohyou <cohyou@ironoir.io>
2018-11-01 16:01:18 +0900

    making flask sample endpoint...

--- setup.py
+++ setup.py
@@ -2,5 +2,10 @@ from setuptools import setup
 
 setup(
     name='kskp.web',
-    packages=['kskp.web']
+    packages=['kskp.web'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'flask_mail',
+    ],
 )
