e5fe45c2ab94e7a66b1d42f4e1f7e2d412772611
Takahiro Izuchi <izuchi@impath.jp>
2018-12-24 03:12:00 +0900

    update component

--- src/js/components/shared/AddButton/index.jsx
+++ src/js/components/shared/AddButton/index.jsx
@@ -6,13 +6,10 @@ import classnames from 'classnames'
 type Props = {
   onClick: Function;
   children: React.Children;
-  disabled: boolean;
-  icon: string;
-  danger: boolean;
   className?: string;
 }
 
-export default class Button extends React.Component<Props> {
+export default class AddButton extends React.Component<Props> {
   static defaultProps = {
     onClick: () => {},
     disabled: false,
@@ -21,17 +18,11 @@ export default class Button extends React.Component<Props> {
   }
 
   render () {
-    const {onClick, children, disabled, icon, danger,className} = this.props
+    const {onClick, children, className} = this.props
     const iconClass = classnames('material-icons', [style.icon])
-    const buttonClass = classnames(style.button, {[style.danger]: danger,[className]:(className)})
-    const materialIcon = (icon)
-      ? <i className={iconClass} dangerouslySetInnerHTML={{__html: icon}}></i>
-      : null
-    return <button type="button" className={buttonClass} disabled={disabled} onClick={onClick}>
-      {materialIcon}
-      <div className={style.text}>
-        {children}
-      </div>
-    </button>
+    return <div className={style.addButton} onClick={onClick}>
+      <i className={iconClass}>add_circle_outline</i>
+      {children}
+    </div>
   }
 }
