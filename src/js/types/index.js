ce0c91ab82e780fb605ea40f1845ae3b28d27e79
hajaehoon <ha.jaehoon@ksk-anl.com>
2019-02-19 18:37:47 +0900

    メモについて下記の機能を追加
    ① メモボタンを押す
    ② 画面上、どこかにメモオブジェクトが追加される

--- src/js/types/index.js
+++ src/js/types/index.js
@@ -5,7 +5,7 @@ import DataFrameStepModel from '../model/Step/DataFrameStepModel'
 import * as React from 'react'
 import CommandModel from '../model/Command/CommandModel'
 
-export type StepModelType = CommandStepModel|SubFlowStepModel|DataFrameStepModel
+export type StepModelType = CommandStepModel|SubFlowStepModel|DataFrameStepModel|CommentStepModel
 export type CommandParamType = {
   label: string;
   name: string;
@@ -108,4 +108,8 @@ export type DataFrameDetailType = {
   contents: {};
   numberOfLines: string;
   lastModifiedAt: string;
+}
+
+export type CommentDetailType = {
+  
 }
