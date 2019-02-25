08675a213307a1935a653f68b1c8676ebfc5ba71
hajaehoon <ha.jaehoon@ksk-anl.com>
2019-02-25 12:41:35 +0900

    fix memo title

--- src/js/components/ProfileContainer/index.jsx
+++ src/js/components/ProfileContainer/index.jsx
@@ -86,7 +86,6 @@ export default class ProfileContainer extends React.Component<Props, State> {
   }
 
   onSubmit(formState){
-    console.log(formState)
     this.setState({is_loading: true})
 
     const body = {
@@ -108,7 +107,6 @@ export default class ProfileContainer extends React.Component<Props, State> {
 
     HttpUtil.put('profile/' + inject_user_id,body).then((response) => {
       const json = response.data
-      console.log(json)
       this.setState(
         {is_loading: false})
     }).catch((error) => {
