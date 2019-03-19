4bd0847351f9a64b20155fea853b7ae8bb7a04ef
Takahiro Izuchi <izuchi@impath.jp>
2019-03-19 13:00:46 +0900

    ファイルアップロードの変更

--- src/js/utils/APIUtil.js
+++ src/js/utils/APIUtil.js
@@ -54,14 +54,40 @@ class APIUtil {
     return axios.delete(url, data, merged_config)
   }
 
-  fileupload(file:File,fileName:string){
+  frameUpload(file:File,fileName:string,label:string,parentUUID:string){
+    const options = {
+      headers: { 'enctype': 'multipart/form-data' }
+    }
+
+    let formData:FormData = new FormData();
+    console.log(parentUUID)
+    console.log(label)
+    formData.append('file', file)
+    if(fileName){
+      formData.append('file_name', fileName)//TODO 将来的にはなくなる？？
+    }
+    if(label){
+      formData.append('label', label)
+    }
+    if(parentUUID){
+      formData.append('parent', parentUUID)
+    }
+
+    return this.post('frames', formData,options)
+  }
+  documentUpload(file:File,label:string,parentUUID:string){
     const options = {
       headers: { 'enctype': 'multipart/form-data' }
     }
 
     let formData:FormData = new FormData();
     formData.append('file', file)
-    formData.append('file_name', fileName)
+    if(label){
+      formData.append('label', label)
+    }
+    if(parentUUID){
+      formData.append('parent', parentUUID)
+    }
 
     return this.post('frames', formData,options)
   }
