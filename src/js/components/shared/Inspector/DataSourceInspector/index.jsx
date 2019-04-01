172f4617739448bcf0000f8c0a8256de91f392b7
hajaehoon <ha.jaehoon@ksk-anl.com>
2019-04-01 12:33:42 +0900

    hotfix #164 ラベルが変更されない件

--- src/js/components/shared/Inspector/DataSourceInspector/index.jsx
+++ src/js/components/shared/Inspector/DataSourceInspector/index.jsx
@@ -456,7 +456,7 @@ class DataSourceInspector extends React.Component<FlowEditorProps,State> {
       </div>
     }
 
-
+    // FIXIT onBlurTitle to onChange #164
     return <BaseInspector header={""}  label={selected_step.label} {...this.props} onBlurTitle={(e)=>this.onBlurTitle(e)} onHide={()=>this.onHide()}>
       {content}
     </BaseInspector>
