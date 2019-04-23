10ff6884b03f2fd7a07984466a7ac559b53088db
hiroshi <hiroshi.tanaka@ksk-anl.com>
2019-04-23 16:09:30 +0900

    Resolve conflict files.

--- kskp/utils/kskp_json_encoder.py
+++ kskp/utils/kskp_json_encoder.py
@@ -8,6 +8,9 @@ class KSKPJSONEncoder(JSONEncoder):
     ライブラリにおいて定義したクラスのJSONへのデコード処理を定義する
     """
     def default(self, obj):
+        ## types = [Store, Folder, Frame]
+        ## if any(isinstance(obj, tp) for tp in types):
+        ## とかの方がいいのでは？
         if isinstance(obj, Store):
             return obj.to_json()
         elif isinstance(obj, Datum):
