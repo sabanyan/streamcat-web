baccfa47d7d3633aeff7b3e7bcb082c2680cb373
yasuyuki okasaka <yasuyuki.okasaka@ksk-anl.com>
2019-05-29 17:57:11 +0900

    add visualize api(work table only) and templates

--- kskp/web/__init__.py
+++ kskp/web/__init__.py
@@ -1,18 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from .frames.api import frame_api
-from .api import api
-
-PREFIX = '/api/v0'
 
 app = Flask('kskp.web')
 
-app.register_blueprint(api, url_prefix=PREFIX)
-app.register_blueprint(frame_api, url_prefix=PREFIX)
-
+# jsonify関数を使うときにUTF-8として返却できるようにするための設定
+app.config['JSON_AS_ASCII'] = False
+# jsonify関数を使ってJSON形式で返すと勝手に並び順がソートされてしまうので、それを無効にする
+app.config["JSON_SORT_KEYS"] = False
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-db = SQLAlchemy(app)
+from kskp.web.views import visualize
+app.register_blueprint(visualize.mod, url_prefix='/')
+
+from .api import api
+from .frames import frame_api
+
+PREFIX = '/api/v0'
+
+app.register_blueprint(api, url_prefix=PREFIX)
+app.register_blueprint(frame_api, url_prefix=PREFIX)
 
 if __name__ == '__main__':
     app.run()
--- kskp/web/api.py
+++ kskp/web/api.py
@@ -1,17 +1,17 @@
 from pathlib import Path
 from flask import Blueprint, jsonify, request, jsonify
 
-# from kskp.store import CommandsPathFileSource
-# from kskp.web.core import CommandsPathLink
-#
+from kskp.store import CommandsPathFileSource
+from kskp.web.core import CommandsPathLink
+
 api = Blueprint('api', __name__)
-#
-# @api.route('/commands')
-# def fetch_commands():
-#     """
-#     コマンド定義の一覧を返す
-#     """
-#
-#     link = CommandsPathLink(CommandsPathFileSource())
-#
-#     return jsonify({'success': True, 'data': link.resolve()})
+
+@api.route('/commands')
+def fetch_commands():
+    """
+    コマンド定義の一覧を返す
+    """
+
+    link = CommandsPathLink(CommandsPathFileSource())
+
+    return jsonify({'success': True, 'data': link.resolve()})
