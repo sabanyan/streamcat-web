2add6024b4fb04eece4a4bc31a2bffd9f20c62b8
hajaehoon <ha.jaehoon@ksk-anl.com>
2019-07-16 12:48:54 +0900

    add paramLsit (multipleRow)

--- src/js/components/shared/Inspector/Param/ParamString/index.jsx
+++ src/js/components/shared/Inspector/Param/ParamString/index.jsx
@@ -18,17 +18,15 @@ export default class ParamString extends Param {
 
   render () {
     //FIXIT: 将来、onBuildが要らなくなったら、onBuildは消した方がいいかも
-    const {param, onBuild, events, defaultValue, refValue, disabled} = this.props
+    const {param, onBuild, events, defaultValue, refValue, disabled, noLabel} = this.props
     let inputRef = refValue
     if (onBuild) {
       inputRef = element => onBuild(param, element)
     }
-
     const label = (param.label) ? param.label : param.name
+    const labelContainer = (noLabel) ? null : <label>{label}</label> 
     return <div>
-      <label>
-        {label}
-      </label>
+      {labelContainer}
       <input name={param.name} type="text" className="form-control" placeholder={param.name} defaultValue={defaultValue}
              ref={inputRef} disabled={disabled} paramtype={param.type}  {...events} ></input>
     </div>
--- src/js/components/shared/Inspector/ParamsForm/index.jsx
+++ src/js/components/shared/Inspector/ParamsForm/index.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { ParamUtil } from 'Utils/index'
 import CommandModel from 'Model/Command/CommandModel'
 import type { CommandParamType } from 'Types/index'
+import { AddButton, Button } from 'Shared/Input'
 import classnames from 'classnames'
 import style from './style.scss'
 
@@ -80,6 +81,47 @@ export default class ParamsForm extends React.Component<Props> {
     return null
   }
 
+  onClickAddParamRow(e, param, v) {
+    const { events } = this.props
+ 
+    if (v && Array.isArray(v) && events && events.onUpdate) {
+      events.onUpdate((step) => {
+        if (step.args) {
+          v.push("")
+          step.args[param.name] = v
+        }
+        return step
+      })
+    }
+  }
+
+  onChangeParamRow(e, param, index) {
+    const { events } = this.props
+    if (param && param.multipleRow && events && events.onUpdate) {
+      events.onUpdate((step) => {
+        if (step.args) {
+          step.args[param.name][index] = e.currentTarget.value
+        }
+        return step
+      })
+    }
+  }
+
+  onClickDeleteParamRow(param, v, index) {
+    const { events } = this.props
+    if (events && events.onUpdate) {
+      events.onUpdate((step) => {
+        if (step.args) {
+          step.args[param.name] = step.args[param.name].filter((value, filterIndex) => {
+            return (filterIndex !== index)
+          })
+          step.invalidMessage = [""]
+        }
+        return step
+      })
+    }
+  }
+  
   render () {
     const {params, args, invalids, command, onBuild, events, headers} = this.props
     let isPresence = false
@@ -88,28 +130,59 @@ export default class ParamsForm extends React.Component<Props> {
     const paramsForm = params.map((param, index) => {
 
       //入力値 or 初期値を取得する
-      const value = this.getDefaultValueOrArgsValue(args, param)
-
+      let value = this.getDefaultValueOrArgsValue(args, param)
       //必須
       if (command) {
         isPresence = this.isPresence(command, param)
       }
-
       //型に種別に応じたDOMElementの取得
       let paramElement
       //FIXIT: 将来、onBuildが要らなくなったら、onBuildは消した方がいいかも
-
-      paramElement = ParamUtil.getParamElement(param, onBuild, events, value, param.name, headers)
+      let paramElements = []
+      let addButton = null
+      if (param.multipleRow) {
+        if (!value) {
+          value = [""]
+        }
+        if (!(Array.isArray(value))) {
+          value = [value]
+        }
+        value.forEach((v, index) => {
+          let newEvent = {
+            onChange : (e) => this.onChangeParamRow(e, param, index)
+          }
+          let noLabel = (index == 0) ? false : true
+          paramElement = <div key={"param" + index}>
+            <div  className={style.left}>
+              {ParamUtil.getParamElement(param, onBuild, newEvent, v, param.name, headers, noLabel)}
+            </div>
+            <div className={(index === 0) ? style.topRight : style.right}>
+              <Button danger={true} onClick={() => this.onClickDeleteParamRow(param, v, index)}>削除</Button>
+            </div>
+          </div>
+        
+          paramElements.push(paramElement)
+        })
+        addButton = <div className={style.addButton}>
+          <AddButton onClick={(e) => this.onClickAddParamRow(e, param, value)}></AddButton>
+        </div>
+      } else {
+        paramElement = ParamUtil.getParamElement(param, onBuild, events, value, param.name, headers) 
+        paramElements.push(paramElement)
+      }
       //入力エラーメッセージ
       const invalidMessageEelement = this.getInvalidMessageElement(invalids[param.name])
 
-      return <div key={index} className={classnames('mb-8px', {
-        [style.presence]: isPresence,
-        [style.invalid]: (invalidMessageEelement)
-      })}>
-        {paramElement}
-        {invalidMessageEelement}
-      </div>
+      return <React.Fragment>
+        <div key={index} className={classnames('mb-8px', {
+          [style.presence]: isPresence,
+          [style.invalid]: (invalidMessageEelement)
+        })}>
+          {paramElements}
+          {invalidMessageEelement}
+          {addButton}
+        </div>
+      </React.Fragment>
     })
 
     return paramsForm
