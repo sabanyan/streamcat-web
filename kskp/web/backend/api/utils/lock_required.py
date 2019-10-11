d1f16d59a894110454f9049cb35dd8d550e7beb7
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2019-10-11 14:02:03 +0900

    Replace the lock manager onto app.config

--- kskp/web/backend/api/utils/lock_required.py
+++ kskp/web/backend/api/utils/lock_required.py
@@ -1,6 +1,6 @@
 import functools
 from flask import request
-# from kskp.web.backend import lock_manager
+from kskp.web.backend import app
 
 def lock_required(func):
     """
@@ -8,9 +8,10 @@ def lock_required(func):
     """
     @functools.wraps(func)
     def wrapper(**kwargs):
-        if 'lock' not in request.json:
+        if request.json is None or 'lock' not in request.json:
             raise Exception('ロックのUUIDを指定してください')
         lock_uuid = request.json['lock']
+        lock_manager = app.config['LOCK_MANAGER'] 
         if not lock_manager.contains(lock_uuid):
             raise Exception('対象データをロックしてから更新・削除してください')
         return func(**kwargs)
