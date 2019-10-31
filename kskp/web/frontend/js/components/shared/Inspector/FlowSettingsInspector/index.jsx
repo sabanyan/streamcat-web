f068a48750f2083731e4314bdf8755b040f040df
hajaehoon <ha.jaehoon@ksk-anl.com>
2019-10-31 13:21:50 +0900

    fix bug (invisible subflow label)

--- kskp/web/frontend/js/components/shared/Inspector/FlowSettingsInspector/index.jsx
+++ kskp/web/frontend/js/components/shared/Inspector/FlowSettingsInspector/index.jsx
@@ -49,6 +49,7 @@ class FlowSettingsInspector extends React.Component<FlowSettingsInspectorProps> 
     let params = []
     this.paramRefs.forEach(elem => {
       let param: SubFlowParamType = {}
+      param['label'] = elem.value
       param['name'] = elem.value
       param['type'] = 'string'
       params.push(param)
@@ -65,7 +66,7 @@ class FlowSettingsInspector extends React.Component<FlowSettingsInspectorProps> 
   onClickAddFlowParam () {
     let {flow} = this.props
     const name = this.setNewParamName('new_param', 1)
-    flow.params.push({name: name, type: 'string'})
+    flow.params.push({label:name, name: name, type: 'string'})
     this.props.updateFlow(flow)
   }
 
