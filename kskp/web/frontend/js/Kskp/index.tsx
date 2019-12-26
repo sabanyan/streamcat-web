c0679b49ef6fd158ef84a98ca566f12552acd5ff
cohyou <cohyou@ironoir.io>
2019-12-26 12:59:56 +0900

    reformat Kskp/index.tsx

--- kskp/web/frontend/js/Kskp/index.tsx
+++ kskp/web/frontend/js/Kskp/index.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react'
-import style from './style.scss'
-import {FlowEditorContainer, FlowListContainer, ProfileContainer, ProjectListContainer, LibraryListContainer} from 'Components/index';
-import NavigationBar from 'Components/shared/Base/NavigationBar/index';
-import { Props as NavigationModelProps } from 'Model/Navigation/NavigationModel'
 import { connect } from 'react-redux'
-import {API} from 'Modules/api/index'
-import { NavigationModel} from 'Model/index';
+
+import { API } from 'Modules/api/index'
+import style from './style.scss'
+
+import { NavigationModel } from 'Model/index';
+import { Props as NavigationModelProps } from 'Model/Navigation/NavigationModel'
+import NavigationBar from 'Components/shared/Base/NavigationBar/index';
+
+import {
+    FlowEditorContainer, FlowListContainer, ProjectListContainer, LibraryListContainer,
+    ProfileContainer
+} from 'Components/index';
 
 export type Props = {
-    viewId  :ViewId
+    viewId: ViewId
 }
 
 export type State = {
@@ -21,7 +27,7 @@ export enum ViewId {
     Library_List,
     Profile,
     Project_List,
-    Undefined,
+    Undefined = -1,
 }
 
 export class Kskp extends React.Component<Props, State> {
@@ -34,53 +40,55 @@ export class Kskp extends React.Component<Props, State> {
         API.REQUEST.GET.NAVIGATION(inject_flow_uuid, inject_project_uuid)
             .then((res) => {
                 this.setState({
-                   nav : API.PARSE.GET.NAVIGATION(res)
+                   nav: API.PARSE.GET.NAVIGATION(res)
                 })
-            })
-       
+            })       
     }
 
-    renderNav() {
-        let nav:NavigationModelProps | undefined
+    renderNavigationBar() {
+        let nav: NavigationModelProps | undefined
         if (this.state && this.state.nav) {
             nav = this.state.nav
         } 
 
-        return <NavigationBar navigation={nav}/>
+        return (
+            <div className={style.nav}>
+                <NavigationBar navigation={nav}/>
+            </div>
+        )
     }
 
-    renderView (viewId:ViewId) {
-        let viewComponent:any = null
+    renderView (viewId: ViewId) {
+        let viewComponent: any = null        
         switch(viewId) {
-            case ViewId.Flow_Editor     : viewComponent = <FlowEditorContainer/>
+            case ViewId.Flow_Editor : viewComponent = <FlowEditorContainer/>
                 break;
-            case ViewId.Flow_List       : viewComponent = <FlowListContainer/>
+            case ViewId.Flow_List   : viewComponent = <FlowListContainer/>
                 break;
-            case ViewId.Library_List    : viewComponent = <LibraryListContainer/>
+            case ViewId.Library_List: viewComponent = <LibraryListContainer/>
                 break;
-            case ViewId.Profile         : viewComponent = <ProfileContainer/>
+            case ViewId.Profile     : viewComponent = <ProfileContainer/>
                 break;
-            case ViewId.Project_List    : viewComponent = <ProjectListContainer/>
+            case ViewId.Project_List: viewComponent = <ProjectListContainer/>
                 break;
-
-                default:
-                    break;
+            default:
+                break;
         }
 
-        return viewComponent
+        return (
+            <div className={style.view}>
+                {viewComponent}
+            </div>
+        )
     }
 
-    render () {
-        const {viewId} = this.props
-        let result:any = null
+    render() {
+        const { viewId } = this.props
+        let result: any = null
         try {
             result = <div className={style.kskp}>
-                <div className={style.nav}>
-                    {this.renderNav()}
-                </div>
-                <div className={style.view}>
-                    {this.renderView(viewId)}
-                </div>
+                {this.renderNavigationBar()}
+                {this.renderView(viewId)}           
             </div>
         } catch(e) {
             console.log(e)
