e43de5201a808d0ef4aa587f3e5b734997ae12b3
hajaehoon <ha.jaehoon@ksk-anl.com>
2020-01-08 14:25:55 +0900

    Add ResponseModule

--- kskp/web/frontend/js/modules/api/response/get/navigation/index.ts
+++ kskp/web/frontend/js/modules/api/response/get/navigation/index.ts
@@ -1,28 +1,28 @@
+import { CommonResponse } from 'Modules/api/core/index';
+
+import { MessageModel } from 'Model/index'
 import { NavigationModel } from "Model/index";
 
-// Navigation
-type RESPONSE_NAVIGATION = {
-    data : {
-        success : boolean,
-        data : {
-            user_id     : string | "",
-            user_name   : string | "",
-            project_uuid: string | "",
-            project_name: string | "",
-            flow_uuid   : string | "",
-            flow_name   : string | ""
-        }
-    }
-}
 
-export function navigation(res:RESPONSE_NAVIGATION):NavigationModel | undefined {
+type NavigationData = {
+    user_id: string | "",
+    user_name: string | "",
+    project_uuid: string | "",
+    project_name: string | "",
+    flow_uuid: string | "",
+    flow_name: string | ""
+}
+export type VizResponse = CommonResponse<undefined, NavigationData>
+export function navigation(res: VizResponse): NavigationModel | undefined {
     let result
-    try {
-        result = new NavigationModel(res.data.data)
-    } catch(e) {
-        console.log(e)
-    } finally {
-        return result
-    }   
+
+    if (!res.data.success) {
+        throw new MessageModel({ title: "Get Navi Exception", messageStatus: "error", code: res.data.code, message: res.data.message })
+    }
+    if (!res.data.data) {
+        throw new MessageModel({ title: "Get /navigaion JSON Parsing Exception", messageStatus: "error", code: res.data.code, message: res.data.message })
+    }
+
+    return new NavigationModel(res.data.data)
 }
 
