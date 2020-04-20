2aecb0331026c2d7a083419b5fd69231f534c9db
hajaehoon <ha.jaehoon@ksk-anl.com>
2020-04-20 16:50:21 +0900

    Fix csvdonwload (do not run delete-locks)

--- kskp/web/frontend/js/components/shared/Inspector/DataSourceInspector/index.jsx
+++ kskp/web/frontend/js/components/shared/Inspector/DataSourceInspector/index.jsx
@@ -176,6 +176,7 @@ class DataSourceInspector extends React.Component<DataSourceInspectorProps, Stat
       let props: CSVModelProps = {
         uuid: selected_step.uuid,
         data: response.data,
+        label: selected_step.label
       }
       const csv: CSVModel = new CSVModel(props)
       csv.handleDownload()
@@ -338,8 +339,8 @@ class DataSourceInspector extends React.Component<DataSourceInspectorProps, Stat
       preview = <Button onClick={(e) => this.onClickPreview(e)}
         icon={'visibility'} disabled={disabled}>プレビュー</Button>
       if (selected_step.hasData()) {
-        const href = APIUtil.apiUrl("files") + "?type=frame&uuid=" + selected_step.uuid + "&ext=csv&label=" + selected_step.label
-        download = <DownloadButton href={href} icon={'get_app'}>CSVダウンロード</DownloadButton>
+        let href = APIUtil.apiUrl("files") + "?type=frame&uuid=" + selected_step.uuid + "&ext=csv&label=" + selected_step.label
+        download = <Button icon={'get_app'} onClick={(e) => this.onClickCSVDownload(e)}>CSVダウンロード</Button>
       }
     }
 
