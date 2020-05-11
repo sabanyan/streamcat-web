00eaf4837b51ad8857ea6b5920c886a5cae716e8
Takahiro Izuchi <izuchi@impath.jp>
2020-05-12 06:08:50 +0900

    rewrite with hooks

--- kskp/web/frontend/js/components/shared/Input/FlatButton/index.tsx
+++ kskp/web/frontend/js/components/shared/Input/FlatButton/index.tsx
@@ -2,41 +2,33 @@ import * as React from "react";
 import style from "./style.scss";
 import classnames from "classnames";
 
-type Props = {
-    onClick: (e: React.MouseEvent<HTMLButtonElement,MouseEvent>) => void;
+interface Props {
+    onClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
     children: React.ReactNode;
-    disabled: boolean;
-    icon: string;
-    danger: boolean;
+    disabled?: boolean;
+    icon?: string;
+    danger?: boolean;
     className?: any;
     primary?: boolean;
 }
 
-export default class FlatButton extends React.Component<Props> {
-    static defaultProps = {
-        onClick: () => {
-        },
-        disabled: false,
-        icon: "",
-        danger: false
-    };
+const FlatButton = (props: Props) => {
+    const {onClick, children, disabled, icon, danger, className, primary} = props;
+    const buttonClass = classnames(style.button, {
+        [style.danger]: danger,
+        [className]: (className),
+        [style.primary]: primary
+    });
+    const baseUrl = "/front_static/";
+    const iconElement = (icon)
+        ? <img className={style.icon} src={baseUrl + "images/icon/" + icon + ".svg"} />
+        : null;
+    return <button type="button" className={buttonClass} disabled={disabled} onClick={onClick}>
+        {iconElement}
+        <div className={classnames({[style.whiteText]: primary, [style.text]: !primary})}>
+            {children}
+        </div>
+    </button>;
+};
 
-    render() {
-        const {onClick, children, disabled, icon, danger, className, primary} = this.props;
-        const iconClass = classnames("material-icons", [style.icon]);
-        const buttonClass = classnames(style.button, {
-            [style.danger]: danger,
-            [className]: (className),
-            [style.primary]: primary
-        });
-        const materialIcon = (icon)
-            ? <i className={iconClass} dangerouslySetInnerHTML={{__html: icon}}/>
-            : null;
-        return <button type="button" className={buttonClass} disabled={disabled} onClick={onClick}>
-            {materialIcon}
-            <div className={classnames({[style.whiteText]: primary, [style.text]: !primary})}>
-                {children}
-            </div>
-        </button>;
-    }
-}
+export {FlatButton};
