96d432be7a5b08bc5d5fb26e2afccc1f211b7cb0
Takahiro Izuchi <izuchi@impath.jp>
2020-05-18 16:13:01 +0900

    remove container and rewrite react hooks

--- kskp/web/frontend/js/components/index.ts
+++ kskp/web/frontend/js/components/index.ts
@@ -2,5 +2,4 @@ export {default as FlowEditorContainer} from 'Components/FlowEditorContainer/ind
 export {default as FlowListContainer} from 'Components/FlowListContainer/index'
 export {default as ProfileContainer} from 'Components/ProfileContainer/index'
 export {default as ProjectListContainer} from 'Components/ProjectListContainer/index'
-export {default as PreviewContainer} from 'Components/PreviewContainer/index'
 export {default as TrashListContainer} from 'Components/TrashListContainer/index'
