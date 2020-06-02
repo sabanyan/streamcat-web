7b47394a3564ac5fd639bc930ed6b258e57150db
Takahiro Izuchi <izuchi@impath.jp>
2020-06-02 15:18:11 +0900

    add support for project type

--- kskp/web/frontend/js/modules/api/request/post/locks/index.tsx
+++ kskp/web/frontend/js/modules/api/request/post/locks/index.tsx
@@ -1,14 +1,14 @@
 import { ApiBase } from 'Modules/api/core/index'
 import { URL } from 'Modules/api/core/url/index';
-import { Url, LockUUID } from "Modules/api/core/types/request";
+import { Url } from "Modules/api/core/types/request";
 
 type Props = Url & {
-    flowUUID: string
+    target: string
 }
 
 export function locks(props: Props) {
     const url = (props.url) ? props.url : URL.POST.locks
-    const data = { target: props.flowUUID }
+    const data = { target: props.target }
 
     return ApiBase.Post(url, data)
 }
