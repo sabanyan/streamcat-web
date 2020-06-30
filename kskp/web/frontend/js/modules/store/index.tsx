2b189c9677a5a624cd8adb570c9ef906f760ab8c
Takahiro Izuchi <izuchi@impath.jp>
2020-06-30 11:13:55 +0900

    fix root reducer

--- kskp/web/frontend/js/modules/store/index.tsx
+++ kskp/web/frontend/js/modules/store/index.tsx
@@ -1,33 +1,41 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux'
 import thunk from 'redux-thunk'
-import { flowEditorReducer, libraryReducer, flowListReducer } from 'Modules/index'
+import { FlowEditorReducer } from 'Modules/index'
 import { reducer as notificationsReducer } from 'reapop'
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { CommonReducer } from 'Modules/reducers/index'
+import {FlowEditorReducerInitialState} from "Modules/application";
+import {CommonReducerInitialState} from "Modules/reducers/common";
 
 
 // default value for notifications
-const defaultNotification = {
+export const defaultNotification = {
     position: 'tr',
     dismissible: true,
     dismissAfter: 2000,
     allowHTML: true,
     closeButton: false
+};
+
+export const rootReducerInitialState = {
+    notifications: [],
+    FlowEditorReducer: FlowEditorReducerInitialState,
+    CommonReducer: CommonReducerInitialState
 }
 
-const reducers = combineReducers({
+const rootReducer = combineReducers({
     notifications: notificationsReducer(defaultNotification),
-    flowEditorReducer,
-    libraryReducer,
-    flowListReducer,
+    FlowEditorReducer,
     CommonReducer
-})
+});
+
+export {rootReducer}
 
 const enhancers = composeWithDevTools(
     applyMiddleware(thunk),
     // other store enhancers if any
 )
 
-const store = createStore(reducers, enhancers)
+const store = createStore(rootReducer, enhancers)
 
 export default store
