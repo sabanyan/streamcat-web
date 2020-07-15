f111ebd05ef0377d2f618632969c76324684be03
Takahiro Izuchi <izuchi@impath.jp>
2020-07-15 14:17:04 +0900

    LibraryInspector のReactHooks対応、TS対応

--- kskp/web/frontend/js/components/shared/Inspector/LibraryInspector/index.tsx
+++ kskp/web/frontend/js/components/shared/Inspector/LibraryInspector/index.tsx
@@ -1,241 +1,221 @@
-import React from 'react'
-import style from '../style.scss'
-import { BaseInspector, Resizer } from 'Shared/Inspector'
-import { LibraryListDataType } from 'Types/index'
-import moment from 'moment/moment'
-import Constants from 'Constants/index'
-import { Button, DownloadButton } from 'Shared/Input'
-import { APIUtil, ModalUtil, StringUtil } from "Utils/index";
-import { LibraryChild } from 'Model/index';
+import React, {useEffect} from "react";
+import style from "../style.scss";
+import {BaseInspector, Resizer} from "Shared/Inspector";
+import {LibraryListDataType} from "Types/index";
+import moment from "moment/moment";
+import Constants from "Constants/index";
+import {Button, DownloadButton} from "Shared/Input";
+import {APIUtil, ModalUtil, StringUtil} from "Utils/index";
+import {LibraryChild} from "Model/index";
 
 type Props = {
-  visualizers: any[];
-  selected: LibraryChild[];
-  lastSelected?: LibraryChild;
-  onClickDelete?: Function;
-  onClickApply?: Function;
-  onClickMove?: Function;
-  onBlurTitle?: Function;
-  onClickEdit?: Function;
-  onClickEditEncoding?: Function;
-  onClickCleanTrash?: Function;
+    visualizers: any[];
+    selected: LibraryChild[];
+    lastSelected?: LibraryChild;
+    onClickDelete?: Function;
+    onClickApply?: Function;
+    onClickMove?: Function;
+    onBlurTitle?: Function;
+    onClickEdit?: Function;
+    onClickEditEncoding?: Function;
+    onClickCleanTrash?: Function;
 }
 
-class LibraryInspector extends React.Component<Props> {
-  display = {
-    label: '名称',
-    encoding: '文字コード',
-    newline: '改行コード',
-    creator: '作成者',
-    createdAt: '作成日時',
-    prevFolderPath: "捨てる前の場所"
-  }
+const LibraryInspector = (props: Props) => {
+    const display = {
+        label: "名称",
+        encoding: "文字コード",
+        newline: "改行コード",
+        creator: "作成者",
+        createdAt: "作成日時",
+        prevFolderPath: "捨てる前の場所"
+    };
 
-  constructor(props: Props) {
-    super(props)
-  }
+    useEffect(() => {
+        //モーダル処理の登録
+        ModalUtil.registerModal({
+            id: Constants.modal.PREVIEW_DATASOURCE, onClickOK: () => {
+                ModalUtil.closeModal(Constants.modal.PREVIEW_DATASOURCE);
+            }
+        });
+    }, []);
 
-  componentWillMount() {
-    //モーダル処理の登録
-    ModalUtil.registerModal({
-      id: Constants.modal.PREVIEW_DATASOURCE, onClickOK: () => {
-        ModalUtil.closeModal(Constants.modal.PREVIEW_DATASOURCE)
-      },
-    })
-  }
+    const onClickPreview = () => {
+        // dataがない（Null)の場合はPreviwボタンは表示しない（render)
+        let {lastSelected} = props;
+        let library: LibraryListDataType = lastSelected;
+        let uuid = library.uuid;
+        // uuidだけでプレビュー
+        window.open("/preview?step_id=" + null + "&dialog=true&frame_uuid=" + uuid + "&title=" + StringUtil.urlEncode(library.label));
+    };
 
-  onClickPreview(e) {
-    // dataがない（Null)の場合はPreviwボタンは表示しない（render)
-    let { lastSelected } = this.props;
-    let library: LibraryListDataType = lastSelected;
-    let uuid = library.uuid
-    // uuidだけでプレビュー
-    window.open('/preview?step_id='+ null + '&dialog=true&frame_uuid=' + uuid + '&title=' + StringUtil.urlEncode(library.label));
-  }
+    const _onClickEdit = () => {
+        const {lastSelected, onClickEdit} = props;
+        if (onClickEdit) onClickEdit(lastSelected);
+    };
 
-  onClickEdit(e) {
-    const { lastSelected, onClickEdit } = this.props
-    if (onClickEdit) onClickEdit(lastSelected)
-  }
+    const renderButtons = (data?: LibraryChild) => {
+        const {selected, onClickDelete, onClickApply, onClickMove, onClickEdit, onClickEditEncoding, onClickCleanTrash} = props;
 
-  renderDownloadButton() {
+        let preview, download, del, apply, move, edit, editEncoding, trashClean;
 
-  }
-  renderButtons(data?: LibraryChild) {
-    const { selected, onClickDelete, onClickApply, onClickMove, onClickEdit, onClickEditEncoding, onClickCleanTrash } = this.props
+        if (selected.length == 1) {
+            // preview button
+            if (data && data.label && data.type === Constants.library.type.frame) {
+                preview = <Button onClick={() => onClickPreview()} icon={"visibility"}>プレビューする</Button>;
+            }
 
-    let preview, download, del, apply, move, edit, editEncoding, trashClean
+            // download button
+            if (data && data.label && data.type === Constants.library.type.frame) {
+                const href = APIUtil.apiUrl("files") + "?type=frame&uuid=" + data.uuid + "&ext=csv&label=" + data.label;
+                download = <DownloadButton href={href} icon={"get_app"}>CSVをダウンロードする</DownloadButton>;
+            }
 
-    if (selected.length == 1) {
-      // preview button
-      if (data && data.label && data.type === Constants.library.type.frame) {
-        preview = <Button onClick={(e) => this.onClickPreview(e)} icon={"visibility"}>プレビューする</Button>
-      }
+            // edit
+            if (onClickEdit && data && data.type === Constants.library.type.database) {
+                edit = <Button onClick={() => _onClickEdit()} icon={"settings"}>設定を開く</Button>;
+            }
 
-      // download button
-      if (data && data.label && data.type === Constants.library.type.frame) {
-        const href = APIUtil.apiUrl("files") + "?type=frame&uuid=" + data.uuid + "&ext=csv&label=" + data.label
-        download = <DownloadButton href={href} icon={"get_app"}>CSVをダウンロードする</DownloadButton>
-      }
+            // apply button
+            if (onClickApply) apply = <Button primary={true} onClick={() => onClickApply(data)}>選択する</Button>;
 
-      // edit
-      if (onClickEdit && data && data.type === Constants.library.type.database) {
-        edit = <Button onClick={(e) => this.onClickEdit(e)} icon={"settings"}>設定を開く</Button>
-      }
+            // editEncoding
+            if (onClickEditEncoding && data && data.type === Constants.library.type.frame) editEncoding =
+                <Button onClick={() => onClickEditEncoding(data)} icon={"edit"}>文字コードを編集する</Button>;
 
-      // apply button
-      if (onClickApply) apply = <Button primary={true} onClick={() => onClickApply(data)}>選択する</Button>
+            // clean trash button
+            if (onClickCleanTrash) trashClean =
+                <Button onClick={(data) => onClickCleanTrash(data)} danger={true} icon={"delete"}>ゴミ箱を空にする</Button>;
 
-      // editEncoding
-      if (onClickEditEncoding && data && data.type === Constants.library.type.frame) editEncoding = <Button onClick={() => onClickEditEncoding(data)} icon={'edit'}>文字コードを編集する</Button>
+        }
 
-      // clean trash button
-      if (onClickCleanTrash) trashClean = <Button onClick={(data) => onClickCleanTrash(data)} danger={true} icon={"delete"}>ゴミ箱を空にする</Button>
+        // 複数選択の場合
+        if (selected.length >= 1) {
+            // delete button
+            if (onClickDelete) del =
+                <Button danger={true} onClick={() => onClickDelete(data)} icon={"delete"}>削除する</Button>;
 
-    }
+            // move button
+            if (onClickMove) move =
+                <Button onClick={(data) => onClickMove(data)} icon={"open_in_browser"}>移動する</Button>;
+        }
 
-    // 複数選択の場合
-    if (selected.length >= 1) {
-      // delete button
-      if (onClickDelete) del = <Button danger={true} onClick={() => onClickDelete(data)} icon={"delete"}>削除する</Button>
+        if (onClickCleanTrash) {
+            // ゴミ箱の場合、削除と移動を非表示にする
+            del = null;
+            move = null;
+        }
 
-      // move button
-      if (onClickMove) move = <Button onClick={(data) => onClickMove(data)} icon={"open_in_browser"}>移動する</Button>
-    }
+        return <React.Fragment>
+            {preview}
+            {download}
+            {edit}
+            {move}
+            {del}
+            {apply}
+            {editEncoding}
+            {trashClean}
+        </React.Fragment>;
+    };
 
-    if(onClickCleanTrash){
-      // ゴミ箱の場合、削除と移動を非表示にする
-      del = null;
-      move = null;
-    }
+    const renderDetail = (data?: LibraryChild) => {
+        let result: any = [];
+        if (!data) return result;
 
-    return <React.Fragment>
-      {preview}
-      {download}
-      {edit}
-      {move}
-      {del}
-      {apply}
-      {editEncoding}
-      {trashClean}
-    </React.Fragment>
-  }
+        // ラベルがあれば、表示する
+        let label;
+        if (data.label) {
+            label = <React.Fragment key={data.label}>
+                <div><label>{display.label}</label></div>
+                <div className={"mb-8px"}>{data.label}</div>
+            </React.Fragment>;
 
-  renderDetail(data?: LibraryChild) {
-    let result: any = []
-    if (!data) return result
+            result.push(label);
+        }
 
-    // ラベルがあれば、表示する
-    let label
-    if (data.label) {
-      label = <React.Fragment key={data.label}>
-        <div><label>{this.display.label}</label></div>
-        <div className={"mb-8px"}>{data.label}</div>
-      </React.Fragment>
+        if (data.type === Constants.library.type.frame) {
+            // 文字コードがあれば、表示する
+            let encoding;
+            if (data.encoding) {
+                encoding = <React.Fragment key={data.encoding}>
+                    <div><label>{display.encoding}</label></div>
+                    <div className={"mb-8px"}>{data.encoding}</div>
+                </React.Fragment>;
 
-      result.push(label)
-    }
+                result.push(encoding);
+            }
 
-    if (data.type === Constants.library.type.frame) {
-      // 文字コードがあれば、表示する
-      let encoding
-      if (data.encoding) {
-        encoding = <React.Fragment key={data.encoding}>
-          <div><label>{this.display.encoding}</label></div>
-          <div className={"mb-8px"}>{data.encoding}</div>
-        </React.Fragment>
+            // 改行コードがあれば、表示する
+            let newline;
+            if (data.newline) {
+                newline = <React.Fragment key={data.newline}>
+                    <div><label>{display.newline}</label></div>
+                    <div className={"mb-8px"}>{data.newline}</div>
+                </React.Fragment>;
 
-        result.push(encoding)
-      }
-
-      // 改行コードがあれば、表示する
-      let newline
-      if (data.newline) {
-        newline = <React.Fragment key={data.newline}>
-          <div><label>{this.display.newline}</label></div>
-          <div className={"mb-8px"}>{data.newline}</div>
-        </React.Fragment>
-
-        result.push(newline)
-      }
-    }
+                result.push(newline);
+            }
+        }
 
 
-    // 作成者があれば、表示する
-    let creator
-    if (data.creator) {
-      creator = <React.Fragment key={data.creator}>
-        <div><label>{this.display.creator}</label></div>
-        <div className={"mb-8px"}>{data.creator}</div>
-      </React.Fragment>
+        // 作成者があれば、表示する
+        let creator;
+        if (data.creator) {
+            creator = <React.Fragment key={data.creator}>
+                <div><label>{display.creator}</label></div>
+                <div className={"mb-8px"}>{data.creator}</div>
+            </React.Fragment>;
 
-      result.push(creator)
-    }
+            result.push(creator);
+        }
 
-    // 作成日時があれば、表示する
-    let createdAt
-    if (data.createdAt) {
-      createdAt = <React.Fragment key={data.createdAt}>
-        <div><label>{this.display.createdAt}</label></div>
-        <div className={"mb-8px"}>{moment(data.createdAt).format(Constants.format.dateTime)}</div>
-      </React.Fragment>
+        // 作成日時があれば、表示する
+        let createdAt;
+        if (data.createdAt) {
+            createdAt = <React.Fragment key={data.createdAt}>
+                <div><label>{display.createdAt}</label></div>
+                <div className={"mb-8px"}>{moment(data.createdAt).format(Constants.format.dateTime)}</div>
+            </React.Fragment>;
 
-      result.push(createdAt)
-    }
+            result.push(createdAt);
+        }
 
-    /*
-    let prevFolderPath
-    if (data.prevFolderPath) {
-      prevFolderPath = <React.Fragment key={data.prevFolderPath}>
-        <div><label>{this.display.prevFolderPath}</label></div>
-        <div className={"mb-8px"}>{data.prevFolderPath}</div>
-      </React.Fragment>
-      result.push(prevFolderPath)
-    }
-    */
+        return <React.Fragment>
+            {result}
+        </React.Fragment>;
+    };
 
-    return <React.Fragment>
-      {result}
-    </React.Fragment>
-  }
+    const renderSelect = (data?: LibraryChild) => {
+        return <div className={style.inspector}>
+            <div className={style.actions}>
+                {renderButtons(data)}
+            </div>
+            <div className={style.detail}>
+                {renderDetail(data)}
+            </div>
+        </div>;
+    };
 
-  renderSelect(data?: LibraryChild) {
-    let content: any = <div className={style.inspector}>
-      <div className={style.actions}>
-        {this.renderButtons(data)}
-      </div>
-      <div className={style.detail}>
-        {this.renderDetail(data)}
-      </div>
-    </div>
+    const renderSelects = (selected: LibraryChild[], data?: LibraryChild) => {
+        return <div className={style.inspector}>
+            <div className={style.actions}>
+                {renderButtons(data)}
+            </div>
+            <div className={style.detail}>
+            </div>
+        </div>;
+    };
 
-    return content
-  }
-
-  renderSelects(selected: LibraryChild[], data?: LibraryChild) {
-    let content = <div className={style.inspector}>
-      <div className={style.actions}>
-        {this.renderButtons(data)}
-      </div>
-      <div className={style.detail}>
-      </div>
-    </div>
-
-    return content
-  }
-
-  render() {
-    const { selected, lastSelected } = this.props
-    let label = (lastSelected && selected.length <= 1) ? lastSelected.label : undefined
-    let content = (selected.length <= 1) ? this.renderSelect(lastSelected) : this.renderSelects(selected, lastSelected)
+    const {selected, lastSelected, onBlurTitle} = props;
+    let label = (lastSelected && selected.length <= 1) ? lastSelected.label : undefined;
+    let content = (selected.length <= 1) ? renderSelect(lastSelected) : renderSelects(selected, lastSelected);
 
     return <Resizer>
-      <BaseInspector label={label} onBlurTitle={this.props.onBlurTitle} disabled={true}>
-        {content}
-      </BaseInspector>
-    </Resizer>
-  }
+        <BaseInspector label={label} onBlurTitle={onBlurTitle} disabled={true}>
+            {content}
+        </BaseInspector>
+    </Resizer>;
+};
 
-}
 
-export default LibraryInspector
+export {LibraryInspector};
