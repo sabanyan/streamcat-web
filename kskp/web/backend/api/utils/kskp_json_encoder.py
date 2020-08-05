1ffec27b97cc397b6764fd29245d072b4d684bcd
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2020-08-05 20:32:06 +0900

    Added system APIs

--- kskp/web/backend/api/utils/kskp_json_encoder.py
+++ kskp/web/backend/api/utils/kskp_json_encoder.py
@@ -4,22 +4,31 @@ from kskp.store import Vis
 from kskp.store import FlowData
 from kskp.core import Datum
 from kskp.store.lock_manager import Lock
+from kskp.store.auth import User, Role
 
 class KSKPJSONEncoder(JSONEncoder):
     """
     ライブラリにおいて定義したクラスのJSONへのデコード処理を定義する
     """
     def default(self, obj):
-        if isinstance(obj, Store):
+        # if isinstance(obj, Store):
+        #     return obj.to_json()
+        # elif isinstance(obj, Vis):
+        #     return obj.to_html()
+        # elif isinstance(obj, Lock):
+        #     return obj.to_json()
+        # elif isinstance(obj, Datum):
+        #     return obj.to_json()
+        # elif isinstance(obj, FlowData):
+        #     return obj.to_json()
+        # else:
+        #     # 上記以外のクラスはデフォルトのデコード処理とする
+        #     return JSONEncoder.default(self, obj)
+
+        if isinstance(obj, (Store, Lock, Datum, FlowData, User, Role)):
             return obj.to_json()
         elif isinstance(obj, Vis):
             return obj.to_html()
-        elif isinstance(obj, Lock):
-            return obj.to_json()
-        elif isinstance(obj, Datum):
-            return obj.to_json()
-        elif isinstance(obj, FlowData):
-            return obj.to_json()
         else:
             # 上記以外のクラスはデフォルトのデコード処理とする
             return JSONEncoder.default(self, obj)
