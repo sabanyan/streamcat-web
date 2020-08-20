5c2999cbf47745d69a78c37192212aae18e69c6d
RyoTsutsui <ryo.tsutsui@ksk-anl.com>
2020-08-20 15:30:30 +0900

    Arrange the appearance

--- kskp/web/backend/api/flow_test.py
+++ kskp/web/backend/api/flow_test.py
@@ -32,19 +32,12 @@ def get_all_uuid():
 
     でOK!（多分）
     """
-    # print(vars(request))
-    # print(request.form["uuid"])
-    # print(request['uuid'])
-    # any_uuid = request.
     flow_uuid_getter = Flow_uuid_getter(g.factory)
     any_uuid = request.args.get('uuid')
-    folder = g.factory.data.find_by_uuid(any_uuid)
-    # first_path = folder.path()
     first_path = g.factory.data.load_root()
-    # first_path = True
 
     list_uuid = flow_uuid_getter.get_folder(first_path, set(), any_uuid)
-    print(list_uuid)
+    # print(list_uuid)
     # return jsonify({'success': True, 'data': flow_uuid_getter.get_folder(first_path,set(), any_uuid)})
    
     # テスト結果の情報を元に、returnするjsonを作成、返却する
@@ -60,14 +53,9 @@ from kskp.core import Datum
 from kskp.store import Store
 from kskp.store.flow_dumper import FlowDumper
 
-# flow_uuids = []
-
-# flow_dumper = FlowDumper(g.factory)
-# return flow_dumper.dump_archive(any_uuid)
 class Flow_uuid_getter(FlowDumper):
     def __init__(self,factory):
         self.factory = factory
-        # self.first_path = g.factory.data.load_root()
 
         import uuid
         self.tmp_path = Path('/tmp')
@@ -76,19 +64,12 @@ class Flow_uuid_getter(FlowDumper):
     
     def get_folder(self,parent_path, gathered_uuids, folder_uuid):
         folder_frame = self.factory.data.find_by_uuid(folder_uuid)
-        # parent_path = folder_frame
-        # parent_path = folder_frame.path()
-        print(folder_frame)
-        print(type(folder_frame))
         parent_path = folder_frame.get_current_folder_path()
-        # parent_path_list = parent_path.get_folder_path()
-
-        print(parent_path)
-        print(type(parent_path))
-        # parent_path = parent_path / Path(parent_path2[0][''])
-
+        
         if not isinstance(folder_frame, Folder):
+            # 対象がflowだった場合
             return {folder_frame.uuid}
+
         children = folder_frame.find_children()
 
         if len(children) == 0:
@@ -115,7 +96,12 @@ def start_assert_cmd(any_uuid):
     あるflowもしくはproject内のflow全てのassertコマンドの実行結果を表示する
     """
     # 入力はuuid,flowか、projectのuuidが対象
+    first_path = g.factory.data.load_root()
+    flow_uuid_getter = Flow_uuid_getter(g.factory)
+    any_uuid = request.args.get('uuid')
+
     # もしprojectのuuidだったならば、その中の全て（サブフォルダ内も含む）のflowのuuidを取り出す
+    list_uuid = flow_uuid_getter.get_folder(first_path, set(), any_uuid)
 
     # それぞれのflowを子フローとし、親フローの入力に小フローの出力pointを配置、これらをmcatで一つの出力にまとめ上げる、といった内容のflow_jsonを作成する(フローネームはtestdata_mcat_flowとし、出力csvのidはd_outとする)
     testdata_mcat_flow = None
