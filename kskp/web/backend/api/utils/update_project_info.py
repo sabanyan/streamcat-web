804368c29c36e1bf0aab71d54314e1fffd34e39c
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2020-09-03 12:28:00 +0900

    Refactored update decorators

--- kskp/web/backend/api/utils/update_project_info.py
+++ kskp/web/backend/api/utils/update_project_info.py
@@ -6,15 +6,22 @@ def update_project_info(func):
     @functools.wraps(func)
     def deco(**kwargs):
         # 参加ユーザの情報を含めるか否か
-        update_members = request.args.get('members') == 'on'
+        if request.args.get('members') != 'on':
+            return func(**kwargs)
 
         # デコレート対象関数の呼び出し
         result = json.loads(func(**kwargs).data.decode())
 
-        if result['success']:
-            project_data = result['data']
-            # Project JSONに情報を追加する
-            _update_project_info_inner(project_data, update_members)
+        # APIの異常終了時は情報を追加しない
+        if not result['success']:
+            return func(**kwargs)
+
+        # Project JSONに情報を追加する
+        project_data = result['data']
+        _update_project_info_inner(project_data)
+        # サブプロジェクトにも情報を追加する
+        for sub_project_data in result['data']['children']:
+            _update_project_info_inner(sub_project_data)
 
         return jsonify(result)
     return deco
@@ -23,23 +30,26 @@ def update_projects_info(func):
     @functools.wraps(func)
     def deco(**kwargs):
         # 参加ユーザの情報を含めるか否か
-        update_members = request.args.get('members') == 'on'
+        if request.args.get('members') != 'on':
+            return func(**kwargs)
 
         # デコレート対象関数の呼び出し
         results = json.loads(func(**kwargs).data.decode())
 
-        if results['success']:
-            # Project JSONに情報を追加する
-            for project_data in results['data']['children']:
-                _update_project_info_inner(project_data, update_members)
+        # APIの異常終了時は情報を追加しない
+        if not results['success']:
+            return func(**kwargs)
+
+        # Project JSONに情報を追加する
+        for project_data in results['data']['children']:
+            _update_project_info_inner(project_data)
 
         return jsonify(results)
     return deco
 
-def _update_project_info_inner(project_data, update_members):
+def _update_project_info_inner(project_data):
     from kskp.core import Datum
-    if update_members:
-        if g.factory.data.exists(project_data['uuid'], type=Datum.PROJECT_TYPE):
-            project = g.factory.data.find_by_uuid(project_data['uuid'], type=Datum.PROJECT_TYPE)
-            project_data.update({'members' : project.get_joined_members()})
+    if g.factory.data.exists(project_data['uuid'], type=Datum.PROJECT_TYPE):
+        project = g.factory.data.find_by_uuid(project_data['uuid'], type=Datum.PROJECT_TYPE)
+        project_data.update({'members' : project.get_joined_members()})
     return project_data
--- kskp/web/backend/api/utils/update_role_info.py
+++ kskp/web/backend/api/utils/update_role_info.py
@@ -6,15 +6,19 @@ def update_role_info(func):
     @functools.wraps(func)
     def deco(**kwargs):
         # 参加ユーザの情報を含めるか否か
-        update_users = request.args.get('users') == 'on'
+        if request.args.get('users') != 'on':
+            return func(**kwargs)
 
         # デコレート対象関数の呼び出し
         result = json.loads(func(**kwargs).data.decode())
 
-        if result['success']:
-            role_data = result['data']
-            # Role JSONに情報を追加する
-            _update_role_info_inner(role_data, update_users)
+        # APIの異常終了時は情報を追加しない
+        if not result['success']:
+            return func(**kwargs)
+
+        # Role JSONに情報を追加する
+        role_data = result['data']
+        _update_role_info_inner(role_data)
 
         return jsonify(result)
     return deco
@@ -23,21 +27,24 @@ def update_roles_info(func):
     @functools.wraps(func)
     def deco(**kwargs):
         # 参加ユーザの情報を含めるか否か
-        update_users = request.args.get('users') == 'on'
+        if request.args.get('users') != 'on':
+            return func(**kwargs)
 
         # デコレート対象関数の呼び出し
         results = json.loads(func(**kwargs).data.decode())
 
-        if results['success']:
-            # Role JSONに情報を追加する
-            for role_data in results['data']:
-                _update_role_info_inner(role_data, update_users)
+        # APIの異常終了時は情報を追加しない
+        if not results['success']:
+            return func(**kwargs)
+
+        # Role JSONに情報を追加する
+        for role_data in results['data']:
+            _update_role_info_inner(role_data)
 
         return jsonify(results)
     return deco
 
-def _update_role_info_inner(role_data, update_users):
-    if update_users:
-        role = g.factory.role.find_by_uuid(role_data['uuid'])
-        role_data.update({'users' : role.get_joined_users()})
+def _update_role_info_inner(role_data):
+    role = g.factory.role.find_by_uuid(role_data['uuid'])
+    role_data.update({'users' : role.get_joined_users()})
     return role_data
