39ac60ce2e4fbeeeb1512fed7abb722e5593f65e
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2020-09-15 21:14:57 +0900

    Fixed HTML render tests

--- kskp/web/backend/views/tests/test_basic.py
+++ kskp/web/backend/views/tests/test_basic.py
@@ -1,70 +1,79 @@
-import os
 import unittest
-import tempfile
 from flask import template_rendered
 from kskp.web.backend import app
-# from kskp.store import model
-from kskp.web.backend.api.tests.utils import setUpUser, setUpProject
+from kskp.core import Datum
+from ...api.tests.api_test_case_base import ApiTestCaseBase
 
-class HtmlTestCase(unittest.TestCase):
+class HtmlTestCase(ApiTestCaseBase):
     def setUp(self):
         app.testing = True
         self.client = app.test_client()
-        self.db_fd, os.environ['SQLITE_PATH'] = tempfile.mkstemp()
-        # with app.app_context():
-        #     model.init_db()
+        # ログインするとsessinにuser_idが格納される
         with self.client.session_transaction() as session:
-            session['user_id'] = 'user_id'
-
+            session['user_id'] = self.USER0.id
 
     def assertRenderTemplate(self, path, file_name):
         templates = []
-        with captured_templates(app, templates):
+        with HtmlTestCase.captured_templates(app, templates):
             response = self.client.get(path)
             template, context = templates[0]
         self.assertEqual(template.name, file_name)
 
+    @staticmethod
+    def captured_templates(app, recorded, **extra):
+        def record(sender, template, context):
+            recorded.append((template, context))
+        return template_rendered.connected_to(record, app)
 
     def test_root(self):
         """
-        ルートから正しくリダイレクトされるかどうかのテスト
+        ルートから正しくリダイレクトされること
         """
-
         result = app.test_client().get('/')
         self.assertEqual(result.status_code, 302)
 
         from urllib.parse import urlparse
         url = urlparse(result.headers['Location'])
-        self.assertEqual(url.path, '/projects')
+        self.assertEqual(url.path, '/library')
 
+    def test_library(self):
+        """
+        ライブラリ画面を表示できること
+        """
+        self.assertRenderTemplate('/library', 'library.html')
 
     def test_projects(self):
         """
-        プロジェクト一覧表示画面を表示するテスト
+        プロジェクトを表示できること
         """
+        # ROOTを取得する
+        root = self.factory.data.load_root()
+        # プロジェクトを作成する
+        result = self.post_uri('/api/v0/projects', {'parent':root.uuid, 'label':'にゃお〜ん😽'}, self.USER0)
+        project_uuid = result['data']['uuid']
+        # 画面が表示できること
+        self.assertRenderTemplate(f'/projects/project_uuid', 'library.html')
 
-        self.assertRenderTemplate('/projects', 'projects.html')
-
-
-    def test_flows(self):
+    def test_cache_folders(self):
         """
-        フロ一覧表示画面を表示するテスト
+        キャッシュフォルダを表示できること
         """
-        # 仮のプロジェクト作成
-        with app.app_context():
-            (user1, project_id, project_uuid) = setUpProject(self)
-        self.assertRenderTemplate('/flows?project=%s' % project_uuid, 'flows.html')
+        self.assertRenderTemplate(f'/folders/{Datum.CACHE_FOLDER_UUID}', 'library.html')
 
+    def test_trashcans(self):
+        """
+        ゴミ箱を表示できること
+        """
+        self.assertRenderTemplate(f'/trashes', 'library.html')
 
     def test_flow_designer(self):
         """
-        フローデザイナが正しく返されるかどうかのテスト
+        フローデザイナ画面が表示できること
         """
-
         self.assertRenderTemplate('/flows/RRRRRRRR-RRRR-4RRR-rRRR-RRRRRRRRRRRR', 'flow_designer.html')
 
-
-def captured_templates(app, recorded, **extra):
-    def record(sender, template, context):
-        recorded.append((template, context))
-    return template_rendered.connected_to(record, app)
+    def test_preview(self):
+        """
+        プレビュー画面が表示できること
+        """
+        self.assertRenderTemplate(f'/preview', 'preview.html')
