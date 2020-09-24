fb7857968fb0780deb7e87609a362b719cdb8642
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2020-09-25 07:22:23 +0900

    Not grant permissions to data everyone at viewing the data

--- kskp/web/backend/api/tests/test_system.py
+++ kskp/web/backend/api/tests/test_system.py
@@ -814,11 +814,6 @@ class SystemTestCase(ApiTestCaseBase):
         # プロジェクトを検索する
         result = self.get_uri(f'/api/v0/projects/{project_uuid}?members=on', self.USER0)
 
-        # 
-        # TODO: 現在はDatum作成時にユーザ管理者に権限を与えているのでエラーになる
-        # 
-        print(result)
-
         # 期待するJSONが返ることを確認する
         self.assertEqual(result['data']['uuid'], project_uuid)
         self.assertEqual(result['data']['type'], 'project')
@@ -829,7 +824,7 @@ class SystemTestCase(ApiTestCaseBase):
         self.assertEqual(result['data']['folderPath'][0]['uuid'], root.uuid)
         self.assertEqual(result['data']['folderPath'][0]['label'], 'ライブラリ')
         # 作成ユーザ
-        self.assertEqual(len(result['data']['members']), 2)
+        self.assertEqual(len(result['data']['members']), 3)
         self.assertEqual(result['data']['members'][0]['uuid'], self.USER0.uuid)
         self.assertEqual(result['data']['members'][0]['email'], self.USER0.email)
         self.assertEqual(result['data']['members'][0]['name'], self.USER0.name)
@@ -837,14 +832,22 @@ class SystemTestCase(ApiTestCaseBase):
         self.assertEqual(result['data']['members'][0]['creator'], self.USER0.creator_str)
         self.assertEqual(result['data']['members'][0]['createdAt'], self.USER0.created_at_str)
         self.assertEqual(result['data']['members'][0]['type'], 'Owner')
+        # システム管理者
+        self.assertEqual(result['data']['members'][1]['uuid'], self.USER1.uuid)
+        self.assertEqual(result['data']['members'][1]['email'], self.USER1.email)
+        self.assertEqual(result['data']['members'][1]['name'], self.USER1.name)
+        self.assertEqual(result['data']['members'][1]['state'], self.USER1.state)
+        self.assertEqual(result['data']['members'][1]['creator'], self.USER1.creator_str)
+        self.assertEqual(result['data']['members'][1]['createdAt'], self.USER1.created_at_str)
+        self.assertEqual(result['data']['members'][1]['type'], 'Owner')
         # 参加ユーザ
-        self.assertEqual(result['data']['members'][1]['uuid'], self.USER2.uuid)
-        self.assertEqual(result['data']['members'][1]['email'], self.USER2.email)
-        self.assertEqual(result['data']['members'][1]['name'], self.USER2.name)
-        self.assertEqual(result['data']['members'][1]['state'], self.USER2.state)
-        self.assertEqual(result['data']['members'][1]['creator'], self.USER2.creator_str)
-        self.assertEqual(result['data']['members'][1]['createdAt'], self.USER2.created_at_str)
-        self.assertEqual(result['data']['members'][1]['type'], 'Reader')
+        self.assertEqual(result['data']['members'][2]['uuid'], self.USER2.uuid)
+        self.assertEqual(result['data']['members'][2]['email'], self.USER2.email)
+        self.assertEqual(result['data']['members'][2]['name'], self.USER2.name)
+        self.assertEqual(result['data']['members'][2]['state'], self.USER2.state)
+        self.assertEqual(result['data']['members'][2]['creator'], self.USER2.creator_str)
+        self.assertEqual(result['data']['members'][2]['createdAt'], self.USER2.created_at_str)
+        self.assertEqual(result['data']['members'][2]['type'], 'Reader')
 
         # ユーザを脱退させる
         result = self.delete_uri(f'/api/v0/projects/{project_uuid}/users/{self.USER2.uuid}', self.USER0)
@@ -852,8 +855,8 @@ class SystemTestCase(ApiTestCaseBase):
         # プロジェクトを検索する
         result = self.get_uri(f'/api/v0/projects/{project_uuid}?members=on', self.USER0)
 
-        # 参加ユーザは1人である
-        self.assertEqual(len(result['data']['members']), 1)
+        # 参加ユーザは2人である
+        self.assertEqual(len(result['data']['members']), 2)
 
         # プロジェクトを削除する
         self.delete_uri(f'/api/v0/projects/{project_uuid}', self.USER0)
