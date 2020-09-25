80ba0320e775f82a7a53bb75e707866f90bfbfd3
izuchy <izuchi@impath.jp>
2020-09-25 16:00:26 +0900

    [ユーザー管理] ユーザー一覧の仮パスワード表示の追加 #83

--- kskp/web/frontend/js/components/shared/Inspector/UserListInspector/index.tsx
+++ kskp/web/frontend/js/components/shared/Inspector/UserListInspector/index.tsx
@@ -1,165 +1,160 @@
-import React from 'react'
+import React, {useState} from 'react'
 import style from '../style.scss'
-import { BaseInspector, Resizer } from 'Shared/Inspector'
-import { UserListUser } from 'Types/index'
+import {BaseInspector, Resizer} from 'Shared/Inspector'
+import {UserListUser} from 'Types/index'
 import moment from 'moment/moment'
 import Constants from 'Constants/index'
-import { Button, DownloadButton } from 'Shared/Input'
-import { APIUtil, ModalUtil, StringUtil } from "Utils/index";
-import { LibraryChild } from 'Model/index';
+import {Button, DownloadButton} from 'Shared/Input'
+import {APIUtil, ModalUtil, StringUtil} from 'Utils/index';
+import {LibraryChild} from 'Model/index';
+import AdminUtil from 'Utils/AdminUtil';
 
-type Props = {
-  selected: UserListUser[];
-  lastSelected?: UserListUser;
-  onClickDelete?: Function;
-  onBlurTitle?: Function;
-  onClickEdit?: Function;
+interface Props {
+    selected: UserListUser[];
+    lastSelected?: UserListUser;
+    onClickDelete?: Function;
+    onBlurTitle?: Function;
+    onClickEdit?: Function;
 }
 
-class UserListInspector extends React.Component<Props> {
-  display = {
+const display = {
     email: 'E-mail',
     projects: '所属プロジェクト',
     status: 'ステータス',
     admin_types: 'KSKP 管理権限',
     password: '仮パスワード',
-  }
+}
 
-  constructor(props: Props) {
-    super(props)
-  }
 
-  componentWillMount() {
-    //モーダル処理の登録
-    // ModalUtil.registerModal({
-    //   id: Constants.modal.PREVIEW_DATASOURCE, onClickOK: () => {
-    //     ModalUtil.closeModal(Constants.modal.PREVIEW_DATASOURCE)
-    //   },
-    // })
-  }
+const UserListInspector = (props: Props) => {
 
-  onClickEdit(e) {
-    const { lastSelected, onClickEdit } = this.props
-    if (onClickEdit) onClickEdit(lastSelected)
-  }
 
-  renderButtons(data?: UserListUser) {
-    const { selected, onClickDelete} = this.props
-
-    let del
-
-    // 複数選択の場合
-    if (selected.length >= 1) {
-      // delete button
-      if (onClickDelete) del = <Button danger={true} onClick={() => onClickDelete(data)} icon={"delete"}>削除する</Button>
-    }
-    return <React.Fragment>
-      {del}
-    </React.Fragment>
-  }
-
-  renderDetail(data?: UserListUser) {
-    let result: any = []
-    console.log("renderDetail",data);
-    if (!data) return result
-
-    if (data.email) {
-      let label = <React.Fragment key={data.email}>
-        <div><label>{this.display.email}</label></div>
-        <div className={"mb-8px"}>{data.email}</div>
-      </React.Fragment>
-      result.push(label)
+    const onClickEdit = (e) => {
+        const {lastSelected, onClickEdit} = props
+        if (onClickEdit) onClickEdit(lastSelected)
     }
 
-    if (data.status) {
-      let status = <React.Fragment key={data.status}>
-        <div><label>{this.display.status}</label></div>
-        <div className={"mb-8px"}>{data.status}</div>
-      </React.Fragment>
-      result.push(status)
+    const renderButtons = (data?: UserListUser) => {
+        const {selected, onClickDelete} = props
+
+        let del
+
+        // 複数選択の場合
+        if (selected.length >= 1) {
+            // delete button
+            if (onClickDelete) del =
+                <Button danger={true} onClick={() => onClickDelete(data)} icon={'delete'}>削除する</Button>
+        }
+        return <React.Fragment>
+            {del}
+        </React.Fragment>
     }
-    //
-    //   // 改行コードがあれば、表示する
-    //   let newline
-    //   if (data.newline) {
-    //     newline = <React.Fragment key={data.newline}>
-    //       <div><label>{this.display.newline}</label></div>
-    //       <div className={"mb-8px"}>{data.newline}</div>
-    //     </React.Fragment>
-    //
-    //     result.push(newline)
-    //   }
-    // }
-    //
-    //
-    // // 作成者があれば、表示する
-    // let creator
-    // if (data.creator) {
-    //   creator = <React.Fragment key={data.creator}>
-    //     <div><label>{this.display.creator}</label></div>
-    //     <div className={"mb-8px"}>{data.creator}</div>
-    //   </React.Fragment>
-    //
-    //   result.push(creator)
-    // }
-    //
-    // // 作成日時があれば、表示する
-    // let createdAt
-    // if (data.createdAt) {
-    //   createdAt = <React.Fragment key={data.createdAt}>
-    //     <div><label>{this.display.createdAt}</label></div>
-    //     <div className={"mb-8px"}>{moment(data.createdAt).format(Constants.format.dateTime)}</div>
-    //   </React.Fragment>
-    //
-    //   result.push(createdAt)
-    // }
 
-    return <React.Fragment>
-      {result}
-    </React.Fragment>
-  }
+    const renderDetail = (data?: UserListUser) => {
+        let result: any = []
 
-  renderSelect(data?: UserListUser) {
-    let content: any = <div className={style.inspector}>
-      <div className={style.actions}>
-        {this.renderButtons(data)}
-      </div>
-      <div className={style.detail}>
-        {this.renderDetail(data)}
-      </div>
-    </div>
+        const [showPassword, setShowPassword] = useState<Boolean>(false)
+        console.log('renderDetail', data);
+        if (!data) return result
 
-    console.log("renderSelect")
-    return content
-  }
+        if (data.email) {
+            let label = <React.Fragment>
+                <div><label>{display.email}</label></div>
+                <div className={'mb-8px'}>{data.email}</div>
+            </React.Fragment>
+            result.push(label)
+        }
 
-  renderSelects(selected: UserListUser[], data?: UserListUser) {
-    let content = <div className={style.inspector}>
-      <div className={style.actions}>
-        {this.renderButtons(data)}
-      </div>
-      <div className={style.detail}>
+        if (data.projects) {
+            let projects = data.projects.map((project) => {
+                return <div className={style.project}>{project.label}</div>
+            })
+            const projectElements = <React.Fragment>
+                <div><label>{display.projects}({data.projects.length})</label></div>
+                <div className={'mb-8px'}>{projects}</div>
+            </React.Fragment>
 
-      </div>
-    </div>
-    console.log("renderSelects")
+            result.push(projectElements)
+        }
 
-    return content
-  }
+        if (data.status) {
+            let status = <React.Fragment>
+                <div><label>{display.status}</label></div>
+                <div className={'mb-8px'}>{AdminUtil.getUserStatus(data.status)}</div>
+            </React.Fragment>
+            result.push(status)
 
-  render() {
-    const { selected, lastSelected } = this.props
+            if (data.status === 'tmp') {
+                const onClickShowPassword = () => {
+                    setShowPassword(true)
+                }
+                let showPasswordElement = <Button onClick={onClickShowPassword}>仮パスワードの表示</Button>
+                result.push(showPasswordElement)
+
+                console.log(data)
+                let tempPasswordLabel = <React.Fragment>
+                    <div><label>{display.password}</label></div>
+                    <div
+                        className={'mb-8px'}>{(showPassword) ? data.password : AdminUtil.replaceAsterisk(data.password.length)}</div>
+                </React.Fragment>
+                result.push(tempPasswordLabel)
+            }
+            if (data.status === 'active') {
+                const onClickPasswordReset = () => {
+
+                }
+                let resetPasswordEelement = <Button danger={true} onClick={onClickPasswordReset}>パスワードリセット</Button>
+                result.push(resetPasswordEelement)
+            }
+        }
+
+
+        return <React.Fragment>
+            {result}
+        </React.Fragment>
+    }
+
+    const renderSelect = (data?: UserListUser) => {
+        let content: any = <div className={style.inspector}>
+            <div className={style.actions}>
+                {renderButtons(data)}
+            </div>
+            <div className={style.full_hr}/>
+            <div className={style.detail}>
+                {renderDetail(data)}
+            </div>
+        </div>
+
+        console.log('renderSelect')
+        return content
+    }
+
+    const renderSelects = (selected: UserListUser[], data?: UserListUser) => {
+        let content = <div className={style.inspector}>
+            <div className={style.actions}>
+                {renderButtons(data)}
+            </div>
+            <div className={style.detail}>
+
+            </div>
+        </div>
+        console.log('renderSelects')
+
+        return content
+    }
+
+
+    const {selected, lastSelected, onBlurTitle} = props
     let label = (lastSelected && selected.length <= 1) ? lastSelected.name : undefined
     console.log(selected.length)
     console.log(lastSelected)
-    let content = (selected.length <= 1) ? this.renderSelect(lastSelected) : this.renderSelects(selected, lastSelected)
+    let content = (selected.length <= 1) ? renderSelect(lastSelected) : renderSelects(selected, lastSelected)
 
     return <Resizer>
-      <BaseInspector label={label} onBlurTitle={this.props.onBlurTitle} disabled={true}>
-        {content}
-      </BaseInspector>
+        <BaseInspector label={label} onBlurTitle={onBlurTitle} disabled={true}>
+            {content}
+        </BaseInspector>
     </Resizer>
-  }
 
 }
 
---
+++ kskp/web/frontend/js/utils/AdminUtil.ts
@@ -0,0 +1,18 @@
+export default class AdminUtil {
+    static getUserStatus = (status: string): string => {
+        switch (status) {
+            case 'active':
+                return '登録済';
+            case 'tmp':
+                return '仮登録';
+            case 'delete':
+                return '削除済';
+        }
+        return '';
+    };
+
+    static replaceAsterisk = (length: number): string => {
+        const asteriskStr = [...Array(length)].map((index) => {return "*"});
+        return asteriskStr.toString().replace(/,/g, '')
+    }
+}
