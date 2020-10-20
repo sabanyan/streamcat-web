3e1b35ce5e08ef4310a1be42d40d5633f663d8d5
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2020-10-20 20:42:19 +0900

    Add setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 setup(
     name='kskp.web',
     packages=['kskp.web'],
+    description='Web Frontend of KSKP',
+    url='https://www.ksk-anl.com/products/kskp',
+    version_format='{tag}',
+    setup_requires=['setuptools-git-version'],
     include_package_data=True,
     install_requires=[
         'flask',
