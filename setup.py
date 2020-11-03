b6e137d4d708c33035498ee107ef099c0366911a
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2020-11-03 13:48:02 +0900

    Suppressed warning message to console

--- setup.py
+++ setup.py
@@ -11,6 +11,9 @@ setup(
         'flask_mail',
         'numpy',
         'scipy',
+        # param 1.10.0では以下のWarningが多量に表示される、bokehが参照している?
+        # "WARNING:param.Dimension: Use method 'get_param_values' via param namespace"
+        'param<=1.9.3'
         'pandas==0.24.2',
         'sklearn',
         'holoviews==1.12.7',
