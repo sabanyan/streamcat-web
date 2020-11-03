a322b7f143d8d9aa37f4bcb4a0352d9246df6a4e
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2020-11-03 21:04:41 +0900

    Sorry, forgeted to append a comma

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         'scipy',
         # param 1.10.0では以下のWarningが多量に表示される、bokehが参照している?
         # "WARNING:param.Dimension: Use method 'get_param_values' via param namespace"
-        'param<=1.9.3'
+        'param<=1.9.3',
         'pandas==0.24.2',
         'sklearn',
         'holoviews==1.12.7',
