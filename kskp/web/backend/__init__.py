5ecdfa1ebf5ee25b7096d4086a5186c0d76e9cd5
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2020-12-07 16:11:58 +0900

    Enabled some security settings of Flask

--- kskp/web/backend/__init__.py
+++ kskp/web/backend/__init__.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, session
+from flask.helpers import url_for
 
 app = Flask('kskp.web.backend')
+
+# SessionのCookieを署名するための秘密鍵
+# SessionのCookieを秘密鍵で署名して改竄を防ぐ
+# TODO: 毎回ランダムに変更した方がいい？
 app.secret_key = '-jm624cqpry89e'
 
 # コマンド一覧で表示させるコマンドのリスト
@@ -9,6 +14,25 @@ app.config['VISIBLE_COMMANDS_JSON'] = ['mcmd', 'kcmd', 'pcmd']
 app.config['JSON_AS_ASCII'] = False
 # jsonify関数を使ってJSON形式で返すと勝手に並び順がソートされてしまうので、それを無効にする
 app.config['JSON_SORT_KEYS'] = False
+
+# SessionのCookieの名前
+app.config['SESSION_COOKIE_NAME'] = 'S'
+# True: WebブラウザはJavaScriptによるSessionのCookieへのアクセスが禁止される
+app.config['SESSION_COOKIE_HTTPONLY'] = True
+# True: WebブラウザのSessionのCookieの送信はHTTPSによる送信だけに制限される
+# "http://www.host.com:443"のようなURLにアクセスさせてSessionのCookieを平文で送信することを防ぐ
+app.config['SESSION_COOKIE_SECURE'] = False
+# Cross-Site Request Forgeries対策
+# Lax   : 他ドメインへの遷移(top-level navigation)でも、GETメソッドであればSessionのCookieの送信を許可する
+#         (URLにアクセスしてもSessionのCookieを保持していればログイン画面をスキップできる)
+# Strict: 同一ドメインへの遷移でのみ、SessionのCookieの送信を許可する
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
+# session.permanent=Trueの場合にのみ有効、SessionのCookieの有効期間(秒)を設定する
+app.config['PERMANENT_SESSION_LIFETIME'] = 5 * 24 * 60 * 60
+# True : SessionのCookieを永続化する
+# False: SessionのCookieは永続化しない、Webブラウザが閉じられたらSessionのCookieは削除される
+# TODO: Request-Context内で記述する必要がある
+# session.permanent = False
 
 # flaskのjsonifyによるJSONへのデコード処理を、独自に定義したデコード処理に置き換える
 from .api.utils.kskp_json_encoder import KSKPJSONEncoder
@@ -39,10 +63,6 @@ app.register_blueprint(system.mod, url_prefix=PREFIX)
 # static用
 from kskp.web.frontend import mod
 app.register_blueprint(mod)
-
-from flask import session
-# sessionを31日間保持する場合はTrue
-# session.permanent = True
 
 def run(port=5000):
     app.run(host='0.0.0.0', port=port)
