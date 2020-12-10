52f4bb83d7672b677817cfd042019811db732c88
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2020-12-10 10:41:30 +0900

    Changed KSKP_HTTPS with KSKP_SECURITY_LEVEL

--- kskp/web/backend/__init__.py
+++ kskp/web/backend/__init__.py
@@ -3,8 +3,10 @@ from flask import Flask, session
 
 app = Flask('kskp.web.backend')
 
-# HTTPSで接続する場合はTrue
-is_https=bool(os.getenv('KSKP_HTTPS', 0))
+# 0: セキュリティ設定をしない
+# 1: 基本的なセキュリティ設定をする
+# 2: HTTPS通信を前提としたセキュリティ設定をする
+security_level=int(os.getenv('KSKP_SECURITY_LEVEL', 1))
 
 # SessionのCookieを署名するための秘密鍵
 # SessionのCookieを秘密鍵で署名して改竄を防ぐ
@@ -20,22 +22,46 @@ app.config['JSON_SORT_KEYS'] = False
 
 # SessionのCookieの名前
 app.config['SESSION_COOKIE_NAME'] = 'S'
-# True: WebブラウザはJavaScriptによるSessionのCookieへのアクセスが禁止される
-app.config['SESSION_COOKIE_HTTPONLY'] = True
-# True: WebブラウザのSessionのCookieの送信はHTTPSによる送信だけに制限される
-# "http://www.host.com:443"のようなURLにアクセスさせてSessionのCookieを平文で送信することを防ぐ
-app.config['SESSION_COOKIE_SECURE'] = is_https
-# Cross-Site Request Forgeries対策
-# Lax   : 他ドメインへの遷移(top-level navigation)でも、GETメソッドであればSessionのCookieの送信を許可する
-#         (URLにアクセスしてもSessionのCookieを保持していればログイン画面をスキップできる)
-# Strict: 同一ドメインへの遷移でのみ、SessionのCookieの送信を許可する
-app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
-# session.permanent=Trueの場合にのみ有効、SessionのCookieの有効期間(秒)を設定する
-app.config['PERMANENT_SESSION_LIFETIME'] = 5 * 24 * 60 * 60
-# True : SessionのCookieを永続化する
-# False: SessionのCookieは永続化しない、Webブラウザが閉じられたらSessionのCookieは削除される
-# TODO: Request-Context内で記述する必要がある
-# session.permanent = False
+
+if security_level >= 1:
+
+    @app.after_request
+    def after_request(response):
+        # Webブラウザに対し、コンテンツの取得元をディレクティブに従い制限するよう要求する
+        # https://developer.mozilla.org/ja/docs/Web/HTTP/CSP
+        response.headers['Content-Security-Policy-Report-Only'] = \
+            "default-src 'self'; script-src 'self' 'unsafe-eval'; style-src 'self' 'unsafe-inline'"
+        # Webブラウザに対し、レスポンスヘッダのContent-type以外のタイプで解釈しないように要求する
+        # https://developer.mozilla.org/ja/docs/Web/HTTP/Headers/X-Content-Type-Options
+        response.headers['X-Content-Type-Options'] = 'nosniff'
+        # Webブラウザに対し、<frame>,<iframe>,<embed>,<object>から取得するコンテンツを自身のドメインに制限するよう要求する
+        # https://developer.mozilla.org/ja/docs/Web/HTTP/Headers/X-Frame-Options
+        response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+        # Webブラウザに対し、HTTPSだけで接続することを要求する
+        # https://developer.mozilla.org/ja/docs/Web/HTTP/Headers/Strict-Transport-Security
+        if security_level >= 2:
+            response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
+        # レスポンスを返す
+        return response
+
+    # True: WebブラウザはJavaScriptによるSessionのCookieへのアクセスが禁止される
+    app.config['SESSION_COOKIE_HTTPONLY'] = True
+    # True: WebブラウザのSessionのCookieの送信はHTTPSによる送信だけに制限される
+    # "http://www.host.com:443"のようなURLにアクセスさせてSessionのCookieを平文で送信することを防ぐ
+    if security_level >= 2:
+        app.config['SESSION_COOKIE_SECURE'] = True
+    # Cross-Site Request Forgeries対策
+    # Lax   : 他ドメインへの遷移(top-level navigation)でも、GETメソッドであればSessionのCookieの送信を許可する
+    #         (URLにアクセスしてもSessionのCookieを保持していればログイン画面をスキップできる)
+    # Strict: 同一ドメインへの遷移でのみ、SessionのCookieの送信を許可する
+    app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
+    # session.permanent=Trueの場合にのみ有効、SessionのCookieの有効期間(秒)を設定する
+    app.config['PERMANENT_SESSION_LIFETIME'] = 5 * 24 * 60 * 60
+    
+    # True : SessionのCookieを永続化する
+    # False: SessionのCookieは永続化しない、Webブラウザが閉じられたらSessionのCookieは削除される
+    # TODO: Request-Context内で記述する必要がある
+    # session.permanent = False
 
 # flaskのjsonifyによるJSONへのデコード処理を、独自に定義したデコード処理に置き換える
 from .api.utils.kskp_json_encoder import KSKPJSONEncoder
@@ -66,25 +92,6 @@ app.register_blueprint(system.mod, url_prefix=PREFIX)
 # static用
 from kskp.web.frontend import mod
 app.register_blueprint(mod)
-
-@app.after_request
-def after_request(response):
-    # Webブラウザに対し、HTTPSだけで接続することを要求する
-    # https://developer.mozilla.org/ja/docs/Web/HTTP/Headers/Strict-Transport-Security
-    if is_https:
-        response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
-    # Webブラウザに対し、コンテンツの取得元をディレクティブに従い制限するよう要求する
-    # https://developer.mozilla.org/ja/docs/Web/HTTP/CSP
-    response.headers['Content-Security-Policy-Report-Only'] = \
-        "default-src 'self'; script-src 'self' 'unsafe-eval'; style-src 'self' 'unsafe-inline'"
-     # Webブラウザに対し、レスポンスヘッダのContent-type以外のタイプで解釈しないように要求する
-    # https://developer.mozilla.org/ja/docs/Web/HTTP/Headers/X-Content-Type-Options
-    response.headers['X-Content-Type-Options'] = 'nosniff'
-    # Webブラウザに対し、<frame>,<iframe>,<embed>,<object>から取得するコンテンツを自身のドメインに制限するよう要求する
-    # https://developer.mozilla.org/ja/docs/Web/HTTP/Headers/X-Frame-Options
-    response.headers['X-Frame-Options'] = 'SAMEORIGIN'
-
-    return response
 
 def run(port=5000):
     app.run(host='0.0.0.0', port=port)
