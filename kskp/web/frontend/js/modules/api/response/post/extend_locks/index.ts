338a259e438496a549bdecbe372165abc5c6c065
Takahiro Izuchi <izuchi@impath.jp>
2020-12-22 15:07:31 +0900

    型定義警告の修正

--- kskp/web/frontend/js/modules/api/response/post/extend_locks/index.ts
+++ kskp/web/frontend/js/modules/api/response/post/extend_locks/index.ts
@@ -12,10 +12,5 @@ export function extendLocks(res: Response<ExtendLocksResponse>): ExtendLocksData
     if (!res.data.success) {
         throw new MessageModel({ title: "POST /extend-locks Exception", messageStatus: "warning", code: res.data.code, message: res.data.message })
     }
-    // TODO レスポンスにユーザー情報が含まれるようになったら修正が必要
-    // else if (!res.data.data) {
-    //     alert("dd")
-    //     throw new MessageModel({ title: "POST /extend-locks JSON Parsing Exception", messageStatus: "error", code: res.data.code, message: res.data.message })
-    // }
     return {}
 }
