8cbee9212c1a7617716cb7d8f16f995bba9b6e51
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2021-01-04 12:40:25 +0900

    Added a env variable that set frontend build mode

--- kskp/web/backend/views/utils/make_response.py
+++ kskp/web/backend/views/utils/make_response.py
@@ -5,7 +5,7 @@ def make_response(template_name, **context):
     import uuid
     from flask import render_template, make_response
     from bokeh.resources import INLINE
-    from kskp.web.backend import SECURITY_LEVEL
+    from kskp.web.backend import FRONTEND_BUILD, SECURITY_LEVEL
 
     # Nonce値を生成する
     nonce = str(uuid.uuid4()).upper()[0:6]
@@ -18,11 +18,25 @@ def make_response(template_name, **context):
                                 **context)
     response = make_response(contents)
 
-    if SECURITY_LEVEL >= 1:
+    if FRONTEND_BUILD == 'development':
+        # フロントエンドをdevelopmentモードでビルドする場合はevalの使用を許可する
+        unsafe_eval = "'unsafe-eval'"
+    else:
+        # productionモードでビルドする場合はevalの使用を許可しない
+        unsafe_eval = ""
+
+    if SECURITY_LEVEL == 0:
+        # 制限に違反しても通知のみ行う
+        response.headers['Content-Security-Policy-Report-Only'] = \
+            f"default-src 'self'; script-src 'self' 'nonce-{nonce}' {unsafe_eval}; style-src 'self' 'unsafe-inline'"
+    else:
+        # 
+        # TODO: フロントの対応が完了したら、-Report-Onlyを外す
+        # 
         # Webブラウザに対し、コンテンツの取得元をディレクティブに従い制限するよう要求する
         # https://developer.mozilla.org/ja/docs/Web/HTTP/CSP
         response.headers['Content-Security-Policy-Report-Only'] = \
-            f"default-src 'self'; script-src 'self' 'nonce-{nonce}' 'unsafe-eval'; style-src 'self' 'unsafe-inline'"
+            f"default-src 'self'; script-src 'self' 'nonce-{nonce}' {unsafe_eval}; style-src 'self' 'unsafe-inline'"
 
         # Webブラウザに対し、<frame>,<iframe>,<embed>,<object>から取得するコンテンツを自身のドメインに制限するよう要求する
         # https://developer.mozilla.org/ja/docs/Web/HTTP/Headers/X-Frame-Options
