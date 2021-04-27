e41c2d7fb593479c82360c8532c3b5b5682fb81d
hajaehoon <ha.jaehoon@ksk-anl.com>
2021-04-27 18:21:44 +0900

    update package json & webpack config

--- kskp/webpack.config.js
+++ kskp/webpack.config.js
@@ -1,117 +1,127 @@
-const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const webpack = require('webpack')
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
 
 module.exports = (env) => {
   const mode = (env && env.production) ? 'production' : 'development'
-  return [
-    {
-      mode: mode,
-      entry: './web/frontend/js/common.js',
-      output: {
-        path: `${__dirname}/web/frontend/static/js`,
-        filename: 'common.js',
-      },
-      devtool: 'source-map',
-      module: {
-        rules: [
-          {
-            test: /\.js?$/,
-            use: ['babel-loader'],
-            exclude: /node_modules/,
-          },
-        ],
-      },
-      resolve: {
-        modules: ['node_modules'],
-        extensions: ['.js'],
-      },
-    }
-    ,
-    {
-      // モード値を production に設定すると最適化された状態で、
-      // development に設定するとソースマップ有効でJSファイルが出力される
-      mode: mode,
-      // メインとなるJavaScriptファイル（エントリーポイント）ls
-      entry: ['babel-polyfill', './web/frontend/js/index.tsx'],
-      output: {
-        path: `${__dirname}/web/frontend/static/js`,
-        filename: 'app.js',
-      },
-      devtool: 'source-map',
-      module: {
-        rules: [
-          {
-            test: /\.jsx?$/,
-            use: ['babel-loader'],
-            exclude: /node_modules/,
-          },
-          {
-            test: /\.tsx?$/,
-            use: ['babel-loader', 'ts-loader'],
-            exclude: /node_modules/,
-          },
-          {
-            test: /\.s?css$/,
-            use: ['style-loader', 'css-loader?modules', 'sass-loader'],
-            exclude: /node_modules/,
-          },
-        ],
-      },
-      resolve: {
-        alias: {
-          Components: path.resolve(__dirname, './web/frontend/js/components/'),
-          FlowEditorContainer: path.resolve(__dirname, './web/frontend/js/components/FlowEditorContainer/'),
-          FlowListContainer: path.resolve(__dirname, './web/frontend/js/components/FlowListContainer/'),
-          LibraryContainer: path.resolve(__dirname, './web/frontend/js/components/LibraryContainer/'),
-          ProfileContainer: path.resolve(__dirname, './web/frontend/js/components/ProfileContainer/'),
-          ProjectListContainer: path.resolve(__dirname, './web/frontend/js/components/ProjectListContainer/'),
-          PreviewContainer: path.resolve(__dirname, './web/frontend/js/components/PreviewContainer/'),
-          UserListContainer: path.resolve(__dirname, './web/frontend/js/components/admin/UserListContainer/'),
-          Shared: path.resolve(__dirname, './web/frontend/js/components/shared/'),
-          Constants: path.resolve(__dirname, './web/frontend/js/constants/'),
-          Model: path.resolve(__dirname, './web/frontend/js/model/'),
-          Modules: path.resolve(__dirname, './web/frontend/js/modules/'),
-          Schema: path.resolve(__dirname, './web/frontend/js/schema/'),
-          Types: path.resolve(__dirname, './web/frontend/js/types/'),
-          Utils: path.resolve(__dirname, './web/frontend/js/utils/')
+
+  return {
+    mode: mode,
+    entry: './web/frontend/js/common.js',
+    output: {
+      path: `${__dirname}/web/frontend/static/js`,
+      filename: 'common.js',
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.js?$/,
+          use: [
+            {
+              // Babel を利用する
+              loader: "babel-loader",
+              // Babel のオプションを指定する
+              options: {
+                presets: [
+                  // プリセットを指定することで、ES2020 を ES5 に変換
+                  "@babel/preset-env",
+                ],
+              },
+            },
+          ],
+          exclude: /node_modules/,
         },
-        modules: ['node_modules'],
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-      plugins: [
-        new webpack.DllReferencePlugin({
-          context: __dirname,
-          /**
-           * manifestファイルをロードして渡す
-           */
-          manifest: require(`./web/frontend/static/js/dist/vendor-manifest.json`),
-        }),
-      ],
-      performance: { hints: false }
-    },
-    {
-      mode: mode,
-      entry: './web/frontend/sass/app.scss',
-      output: {
-        path: `${__dirname}/web/frontend/static/css`,
-        filename: 'app.css',
-      },
-      devtool: 'source-map',
-      module: {
-        rules: [
-          {
-            test: /\.scss$/,
-            loader: ExtractTextPlugin.extract({
-              fallback: 'style-loader',
-              use: ['css-loader', 'sass-loader'],
-            }),
-            exclude: /node_modules/,
-          }],
-      },
-      plugins: [
-        new ExtractTextPlugin('app.css'),
       ],
     },
-  ]
-}
+    resolve: {
+      modules: ['node_modules'],
+      extensions: ['.js'],
+    },
+  },
+  {
+    entry: ['babel-polyfill', './web/frontend/js/index.tsx'],
+    output: {
+      path: `${__dirname}/web/frontend/static/js`,
+      filename: 'app.js',
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          use: ['babel-loader'],
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.tsx?$/,
+          use: ['babel-loader', 'ts-loader'],
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: ['style-loader', 'css-loader?modules', 'sass-loader'],
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      alias: {
+        Components: path.resolve(__dirname, './web/frontend/js/components/'),
+        FlowEditorContainer: path.resolve(__dirname, './web/frontend/js/components/FlowEditorContainer/'),
+        FlowListContainer: path.resolve(__dirname, './web/frontend/js/components/FlowListContainer/'),
+        LibraryContainer: path.resolve(__dirname, './web/frontend/js/components/LibraryContainer/'),
+        ProfileContainer: path.resolve(__dirname, './web/frontend/js/components/ProfileContainer/'),
+        ProjectListContainer: path.resolve(__dirname, './web/frontend/js/components/ProjectListContainer/'),
+        PreviewContainer: path.resolve(__dirname, './web/frontend/js/components/PreviewContainer/'),
+        UserListContainer: path.resolve(__dirname, './web/frontend/js/components/admin/UserListContainer/'),
+        Shared: path.resolve(__dirname, './web/frontend/js/components/shared/'),
+        Constants: path.resolve(__dirname, './web/frontend/js/constants/'),
+        Model: path.resolve(__dirname, './web/frontend/js/model/'),
+        Modules: path.resolve(__dirname, './web/frontend/js/modules/'),
+        Schema: path.resolve(__dirname, './web/frontend/js/schema/'),
+        Types: path.resolve(__dirname, './web/frontend/js/types/'),
+        Utils: path.resolve(__dirname, './web/frontend/js/utils/')
+      },
+      modules: ['node_modules'],
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    plugins: [
+      new webpack.DllReferencePlugin({
+        context: __dirname,
+        /**
+         * manifestファイルをロードして渡す
+         */
+        manifest: require(`./web/frontend/static/js/dist/vendor-manifest.json`),
+      }),
+    ],
+    performance: { hints: false }
+  },
+  {
+    mode: mode,
+    entry: './web/frontend/sass/app.scss',
+    output: {
+      path: `${__dirname}/web/frontend/static/css`,
+      filename: 'app.css',
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader'
+          ],
+          exclude: /node_modules/,
+        }],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css'
+      }),
+    ]
+  }
+};
