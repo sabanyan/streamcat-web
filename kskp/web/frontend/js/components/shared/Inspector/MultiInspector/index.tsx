d6258ad6504b284135d9e6e4e1bc10ec9fc2f9cc
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2021-10-05 14:50:07 +0900

    Rewrote MultiInspector and NoteInspector with React Hooks

--- kskp/web/frontend/js/components/shared/Inspector/MultiInspector/index.tsx
+++ kskp/web/frontend/js/components/shared/Inspector/MultiInspector/index.tsx
@@ -2,13 +2,13 @@
 import React from 'react';
 import {BaseInspector} from 'Shared/Inspector';
 import {Button} from 'Shared/Input';
-import {CommandSelector} from "FlowEditorContainer/Command";
+import {CommandSelector} from 'FlowEditorContainer/Command';
 import {CommandStepModel, DataFrameStepModel, SubFlowStepModel} from 'Model/index';
 import {GraphUtil, ModalUtil} from 'Utils/index';
 import Constants from 'Constants/index';
 import {flowEditorReducerInitialState} from 'Modules/flowEditor';
 
-type MultiInspectorProps = {
+type Props = {
     deleteSteps: Function;
     selectSteps: Function;
     nodes: any[];
@@ -22,15 +22,15 @@ type MultiInspectorProps = {
     commandSelectorHidden: boolean;
 }
 
-class MultiInspector extends React.Component<MultiInspectorProps> {
-    onClickDelete(e: any) {
-
+const MultiInspector = (props: Props) => {
+    const onClickDelete = () => {
+        const {deleteSteps, selectSteps} = props;
         ModalUtil.registerModal({
             id: Constants.modal.CONFIRM,
             onClickDone: () => {
-                let {selected_step_ids} = this.props;
-                this.props.deleteSteps(selected_step_ids);
-                this.props.selectSteps();
+                const {selected_step_ids} = props;
+                deleteSteps(selected_step_ids);
+                selectSteps();
                 ModalUtil.closeModal(Constants.modal.CONFIRM);
             }
         });
@@ -43,10 +43,10 @@ class MultiInspector extends React.Component<MultiInspectorProps> {
                 選択されたステップを削除しますか？
             </div>
         });
-    }
+    };
 
-    getNumberOfSelectedDataSources() {
-        const {nodes, selected_step_ids} = this.props;
+    const getNumberOfSelectedDataSources = () => {
+        const {nodes, selected_step_ids} = props;
         let cnt = 0;
         let hasMixedCommand = false; //コマンドが混ざって選択されている場合
         selected_step_ids.forEach((id) => {
@@ -61,47 +61,44 @@ class MultiInspector extends React.Component<MultiInspectorProps> {
         });
         if (hasMixedCommand) return 0;
         return cnt;
+    };
+
+    const {mast, selected_step_ids, addStep, addDataSrcStep, addDataDstStep,
+           selectSteps, addHistory, baseInspectorDisabled, commandSelectorHidden, nodes} = props;
+    const numberOfSelectedDataSources = getNumberOfSelectedDataSources();
+
+    let commandSelector;
+    if (numberOfSelectedDataSources) {
+        commandSelector = <div>
+            <CommandSelector
+                nodes={nodes}
+                mast={mast}
+                numberOfInput={numberOfSelectedDataSources}
+                selected_step_ids={selected_step_ids}
+                addStep={addStep}
+                addDataSrcStep={addDataSrcStep}
+                addDataDstStep={addDataDstStep}
+                selectSteps={selectSteps}
+                addHistory={addHistory} />
+        </div>;
     }
 
-    render() {
-        const {mast, selected_step_ids, addStep, addDataSrcStep, addDataDstStep,
-               selectSteps, addHistory, baseInspectorDisabled, commandSelectorHidden, nodes} = this.props;
-        const numberOfSelectedDataSources = this.getNumberOfSelectedDataSources();
-
-        let commandSelector;
-        if (numberOfSelectedDataSources) {
-            commandSelector = <div>
-                <CommandSelector
-                    nodes={nodes}
-                    mast={mast}
-                    numberOfInput={numberOfSelectedDataSources}
-                    selected_step_ids={selected_step_ids}
-                    addStep={addStep}
-                    addDataSrcStep={addDataSrcStep}
-                    addDataDstStep={addDataDstStep}
-                    selectSteps={selectSteps}
-                    addHistory={addHistory} />
-            </div>;
-        }
-
-        if (commandSelectorHidden) {
-            // コマンドセレクタ非表示の扱いの場合は表示しない
-            commandSelector = null;
-        }
-
-        return <BaseInspector key={JSON.stringify(selected_step_ids)}
-                              header={''}
-                              title={selected_step_ids.length + ' files'}
-                              disabled={baseInspectorDisabled}>
-            <div className="kskp-form">
-                <Button onClick={(e) => this.onClickDelete(e)} danger={true} disabled={baseInspectorDisabled}>
-                    削除する
-                </Button>
-            </div>
-            {commandSelector}
-        </BaseInspector>;
+    if (commandSelectorHidden) {
+        // コマンドセレクタ非表示の扱いの場合は表示しない
+        commandSelector = null;
     }
 
-}
+    return <BaseInspector key={JSON.stringify(selected_step_ids)}
+                          header={''}
+                          title={selected_step_ids.length + ' files'}
+                          disabled={baseInspectorDisabled}>
+        <div className='kskp-form'>
+            <Button onClick={() => onClickDelete()} danger={true} disabled={baseInspectorDisabled}>
+                削除する
+            </Button>
+        </div>
+        {commandSelector}
+    </BaseInspector>;
+};
 
 export default MultiInspector;
