91d2b1b357e1c7b73f73e2bfb1de2c83dd63921b
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2021-10-10 11:59:59 +0900

    Fixed type annotation of ReactDomUtil

--- kskp/web/frontend/js/utils/ReactDomUtil.ts
+++ kskp/web/frontend/js/utils/ReactDomUtil.ts
@@ -2,9 +2,15 @@
 import { renderToString } from 'react-dom/server'
 import * as React from 'react'
 
+type ReactElementType = React.ReactElement<any, string | React.JSXElementConstructor<any>>
+
 export class ReactDomUtil {
-  static renderToString (element: React.ReactElement<any, string | React.JSXElementConstructor<any>>) {
-    return renderToString(element)
-  }
+    static renderToString(element: ReactElementType | {}) {
+        // elementが{}の場合は空文字を返す
+        if (Object.keys(element).length === 0) {
+            return '';
+        }
+        return renderToString(element as ReactElementType);
+    }
 }
 
