e467403ca6c6da14894560157ca3533fef76b2d0
hiroshi80 <hiroshi.tanaka@ksk-anl.com>
2021-10-11 11:40:00 +0900

    Fixed ProfileContainer by canceling merge on 2021-05-28

--- kskp/web/frontend/js/components/ProfileContainer/Profile/index.tsx
+++ kskp/web/frontend/js/components/ProfileContainer/Profile/index.tsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react'
 import style from './style.scss'
 import {APIUtil, ReactDomUtil, ErrorUtil} from 'Utils/index'
 import {ModalManager} from 'Shared/Modal'
-import {Flex, Loader} from 'Shared/Base'
+import {Loader} from 'Shared/Base'
 import {Button, LinkButton, TextField} from 'Shared/Input'
 import {useDispatch} from 'react-redux';
 import {addNotification, removeNotification} from 'reapop';
@@ -106,64 +106,41 @@ const Profile = (props: Props) => {
     const onSubmit = (data) => {
         const formState = data;
         setIsLoading(true);
-        const body = {};
-
-        if(profile && formState['name'] !== profile.name){
-            // 名前に変更があった場合
-            body["name"] = formState['name']
-        }
-
-        if(profile && formState['email'] !== profile.email){
-            // emailに変更があった場合
-            body["email"] = formState['email']
-        }
-
-        if(formState['currentPassword']){
-            // 名前に変更があった場合
-            body["currentPassword"] = formState['currentPassword']
-        }
-
-        if(formState['password']){
-            // 名前に変更があった場合
-            body["password"] = formState['password']
-        }
-
-        if(!Object.keys(body).length){
-            notify({
-                title: "プロフィールの保存エラー",
-                message: "プロフィール設定が更新されていません",
-                status: "error",
-                dismissAfter: 0,
-                closeButton: true
-            });
-            return
+        let body;
+        switch (editing) {
+            case 'name':
+                body = {
+                    name: formState['name']
+                }
+                break;
+            case 'email':
+                body = {
+                    email: formState['email'],
+                    currentPassword: formState['currentPassword'],
+                }
+                break;
+            case 'password':
+                body = {
+                    currentPassword: formState['currentPassword'],
+                    password: formState['password1'],
+                }
         }
         APIUtil.put('users/self', body).then((response) => {
             const json = response.data
             setIsLoading(false)
-            if (!response.data.success) {
-                notify({
-                    title: "プロフィールの保存エラー",
-                    message: ReactDomUtil.renderToString(ErrorUtil.getErrorBody(response)),
-                    status: "error",
-                    dismissAfter: 0,
-                    closeButton: true
-                });
-            } else {
-                notify({
-                    title: "プロフィールの保存",
-                    message: "プロフィールを保存しました。",
-                    status: "success"
-                });
+            if (!json.success) {
+                ErrorUtil.notifyError(notify, 'ユーザー情報更新エラー', json.message);
+                return
             }
-        }).catch((error) => {
             notify({
-                title: 'プロフィールの保存エラー',
-                message: ReactDomUtil.renderToString(ErrorUtil.getErrorBody(error)),
-                status: 'error',
-                dismissAfter: 0,
-                closeButton: true
+                title: 'ユーザー情報を更新しました',
+                message: 'ユーザー情報を更新しました',
+                status: 'success'
             });
+            getProfile();
+            setEditing(null);
+        }).catch((error) => {
+            ErrorUtil.notifyError(notify, 'ユーザー情報更新エラー', error);
             setIsLoading(false)
         });
     }
@@ -178,24 +155,52 @@ const Profile = (props: Props) => {
         <Loader center={true} absolute={true} visible={isLoading}/>
     </div>
 
-
     return <div className={'container mt-40px'}>
         <div className={style.page_title}>
             ユーザー情報変更
         </div>
         <div className={style.property_body}>
             <div className={style.card}>
+                <form onSubmit={(availableUpdateSelf)?handleSubmit(onSubmit):undefined} className={'mb-32px'}>
+                    <div className={'mb-8px'}>
+                        {
+                            (availableUpdateSelf) ?
+                                (editing === 'name') ?
+                                    <label>ユーザー名 <LinkButton
+                                        onClick={() => switchEditing(null)}>キャンセル</LinkButton></label>
+                                    :
+                                    <label>ユーザー名 <LinkButton
+                                        onClick={() => switchEditing('name')}>変更する</LinkButton></label>
+                                :  <label>ユーザー名</label>
+                        }
+                        <TextField readOnly={(editing !== 'name')} placeholder={'ユーザ名'} name={'name'}
+                                   inputRef={register({required: 'ユーザー名を入力してください。'})}/>
+                        {errors.name && <label className={'text-danger'}>{errors.name.message}</label>}
+                    </div>
+                    {
+                        (editing === 'name') ?
+                            <div className={'text-right'}>
+                                <Button disabled={!availableUpdateSelf} submit={true} className={'mr-0'}>保存する</Button>
+                            </div>
+                            :
+                            null
+                    }
+                </form>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div className={'mb-8px'}>
-                        <label>ユーザ名</label>
-                        <Flex alignItems={"center"} justifyContent={"space-between"}>
-                            <Flex width={"100%"}>
-                                <TextField placeholder={'ユーザ名'} defaultValue={profile.name} name={"name"} readOnly={true} inputRef={register}/>
-                            </Flex>
-                            <Flex width={100}>
-                                <LinkButton>変更する</LinkButton>
-                            </Flex>
-                        </Flex>
+                        {
+                            (availableUpdateSelf) ?
+                                (editing === 'email') ?
+                                    <label>メールアドレス <LinkButton
+                                        onClick={() => switchEditing(null)}>キャンセル</LinkButton></label>
+                                    :
+                                    <label>メールアドレス <LinkButton
+                                        onClick={() => switchEditing('email')}>変更する</LinkButton></label>
+                                : <label>メールアドレス</label>
+                        }
+                        <TextField readOnly={(editing !== 'email')} placeholder={'メールアドレス'} type={'email'}
+                                   name={'email'} inputRef={register({required: 'E-mail を入力してください。'})}/>
+                        {errors.email && <label className={'text-danger'}>{errors.email.message}</label>}
                     </div>
                     {
                         (editing === 'email') ?
@@ -280,74 +285,6 @@ const Profile = (props: Props) => {
                         </form>
                         : null
                 }
-            </div>
-        </div>
-
-        <div className={style.property_body}>
-            <div className={style.card}>
-                <div className={'mb-8px'}>
-                    <label>メールアドレス</label>
-                    <Flex alignItems={"center"} justifyContent={"space-between"}>
-                        <Flex width={"100%"}>
-                            <TextField placeholder={'メールアドレス'} defaultValue={profile.email} type={'email'} name={"email"} readOnly={true}  inputRef={register}/>
-                        </Flex>
-                        <Flex width={100}>
-                            <LinkButton>変更する</LinkButton>
-                        </Flex>
-                    </Flex>
-                </div>
-                {
-                    (watch('email') && watch('email') !== profile.email)?
-                        <div className={'mb-8px'}>
-                            <label>現在のパスワード</label>
-                            <TextField placeholder={'現在のパスワード'} type={'password'} name={"currentPassword"} inputRef={register({
-                                required: "Required",
-                                pattern: {
-                                    value: /^[\x20-\x7e]*$/,
-                                    message: "パスワードに利用できない文字列が含まれています"
-                                },
-                                minLength: {
-                                    value: 10,
-                                    message: "パスワードは最低10文字必要です"
-                                },
-                                maxLength: {
-                                    value: 64,
-                                    message: "パスワードは最大64文字までです"
-                                }
-                            })}/>
-                        </div>
-                        :
-                        null
-                }
-            </div>
-        </div>
-        <div className={style.property_body}>
-            <div className={style.card}>
-                <div className={'mb-8px'}>
-                    <label>パスワード</label>
-                    <Flex alignItems={"center"} justifyContent={"space-between"}>
-                        <Flex width={"100%"}>
-                            <TextField placeholder={'パスワード'} type={'password'} name={"password"} readOnly={true} inputRef={register({
-                                required: "Required",
-                                pattern: {
-                                    value: /^[\x20-\x7e]*$/,
-                                    message: "パスワードに利用できない文字列が含まれています"
-                                },
-                                minLength: {
-                                    value: 10,
-                                    message: "パスワードは最低10文字必要です"
-                                },
-                                maxLength: {
-                                    value: 64,
-                                    message: "パスワードは最大64文字までです"
-                                }
-                            })}/></Flex>
-                        <Flex width={100}>
-                            <LinkButton>変更する</LinkButton>
-                        </Flex>
-                    </Flex>
-
-                </div>
             </div>
         </div>
         <ModalManager/>
