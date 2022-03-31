321e29a5f14e31a2c4c63167c2d60f690a9efcb3
hiroshi80 <it.gumbler@gmail.com>
2022-03-31 21:43:41 +0900

    Added Download and Duplicate buttons

---
+++ streamcat/web/frontend/js/components/LibraryContainer/DownloadCsvButton/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useStreamCatNotifications } from "Components/shared/Notification";
+import { APIUtil2 } from "Utils/APIUtil2";
+import { FrameType } from "Model/Library";
+import { Button2 } from "Components/shared/Input";
+
+type Props = {
+    readOnly?:boolean;
+    targets: FrameType[];
+    onSuccess?: (targets:FrameType[]) => void;
+};
+
+export const DownloadCsvButton = (props:Props) => {
+    const {readOnly, targets, onSuccess} = props;
+
+    // 通知ダイアログ
+    const {notifySuccess, notifyError} = useStreamCatNotifications();
+
+    // Frameをダウンロードする
+    const downloadFrame = (frame:FrameType) => {
+        return APIUtil2.downloadFrame(frame.uuid, frame.label).then(() => {
+            notifySuccess('CSVをダウンロードしました', frame.label);
+        }).catch((e) => {
+            notifyError(`CSVダウンロードエラー(${frame.label})`, e.message);
+        });
+    };
+
+    // 全てのFrameをダウンロードする
+    const downloadFrames = (frames:FrameType[]) => {
+        // 全てのFrameをダウンロードした後に、イベントハンドラを呼び出す
+        Promise.all(
+            frames.map(frame => downloadFrame(frame))
+        ).finally(() => {
+            // イベントハンドラを呼び出す
+            onSuccess && onSuccess(frames);
+        });
+    };
+
+    // 選択中の全てのFrameがダウンロード可能の場合にTrue
+    const enabled = targets.map(target => 
+        target.allowlist.download).reduce((prevAllow, allow) => 
+        prevAllow && allow
+    );
+
+    return <Button2 disabled={!enabled || readOnly}
+                    onClick={() => downloadFrames(targets)}>CSVダウンロード</Button2>;
+};
