24ea26915b73ee19eb74a9ebd76840ff301ffe90
hiroshi80 <it.gumbler@gmail.com>
2022-05-25 09:30:19 +0900

    Refactored BreadCrumb with MUI

--- streamcat/web/frontend/js/components/LibraryContainer/Libary/BreadCrumb/index.tsx
+++ streamcat/web/frontend/js/components/LibraryContainer/Libary/BreadCrumb/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import {Fragment} from "react";
 import * as style from "./style.scss";
+import { Breadcrumbs, Link } from '@mui/material';
 import {Text} from "Shared/Base/Texts/Text";
-import {LinkButton} from "Shared/Input";
-import classnames from "classnames";
-import WebUtil from "Utils/WebUtil";
 
 export interface IBreadCrumbsLink {
     uuid: string | null;
@@ -12,40 +9,26 @@ export interface IBreadCrumbsLink {
     url: string;
     type: string;
     current: boolean;
-}
+};
 
 interface Props {
     links?: IBreadCrumbsLink[]
-}
-
-const BreadCrumb = (props: Props) => {
-    const {links} = props;
-    if (!links) {
-        return <div className={style.breadCrumb}>
-            <Text>ライブラリ</Text>
-        </div>;
-    }
-    const linkElements = links.map((link, index) => {
-        let icon: React.ReactNode = <i className={classnames("material-icons")}
-                                       style={{color: "#606c7a"}}>chevron_right</i>;
-        if (index + 1 === links.length) icon = null;
-
-        return <Fragment key={index}>
-            {(link.current) ?
-                <>{link.label}</>
-                :
-                <LinkButton url={link.url} onClick={() => {
-                    WebUtil.navigateURL(link.url);
-                }}>{link.label}</LinkButton>
-            }
-            {icon}
-        </Fragment>;
-    });
-    return <div className={style.breadCrumb}>
-        {linkElements}
-    </div>;
-
-
 };
 
-export {BreadCrumb};
+export const BreadCrumb = (props: Props) => {
+    const links = props.links || [];
+
+    return <Breadcrumbs aria-label='breadcrumb' className={style.breadCrumb}>
+        {
+            links.map(link => (
+                link.current ?
+                // 現在の表示フォルダ
+                <Text>{link.label}</Text>:
+                // 先祖フォルダ
+                <Link href={link.url} underline='none'>
+                    {link.label}
+                </Link>
+            ))
+        }
+    </Breadcrumbs>;
+};
