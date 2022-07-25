9cb8e8f4559c5c4a0859ac7509ca467512171673
hiroshi80 <it.gumbler@gmail.com>
2022-07-25 13:54:08 +0900

    Removed APIUtil.js and axios module

--- streamcat/web/frontend/lib/libdefs.js
+++ streamcat/web/frontend/lib/libdefs.js
@@ -4,7 +4,4 @@
 declare module 'react-redux' { declare module.exports: any; }
 declare module 'eventemitter3' { declare module.exports: any; }
 declare module 'redux' { declare module.exports: any; }
-declare module 'axios' { declare module.exports: any; }
 declare module 'classnames' { declare module.exports: any; }
-
-
