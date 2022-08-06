29d6848b39519a562722e915b05d8548004c406f
hiroshi80 <it.gumbler@gmail.com>
2022-08-06 23:08:21 +0900

    Prepared to Flask 2.3

--- streamcat/web/backend/api/utils/__init__.py
+++ streamcat/web/backend/api/utils/__init__.py
@@ -1,4 +1,4 @@
-from .scat_json_encoder import SCatJSONEncoder
+from .scat_json_provider import SCatJSONProvider
 from .scat_log_formatter import SCatLogFormatter, XHRFilter
 from .token import make_access_token, make_refresh_token, expired_soon, decode_token
 from .login_required_api import login_required_api, get_token_from_auth_header
--- streamcat/web/backend/api/utils/scat_json_provider.py
+++ streamcat/web/backend/api/utils/scat_json_provider.py
@@ -1,10 +1,11 @@
-from flask.json import JSONEncoder
+from flask.json.provider import DefaultJSONProvider
 
-class SCatJSONEncoder(JSONEncoder):
+class SCatJSONProvider(DefaultJSONProvider):
     """
-    ライブラリにおいて定義したクラスのJSONへのデコード処理を定義する
+    ライブラリにおいて定義したクラスのJSONへのエンコード処理を定義する
     """
-    def default(self, obj):
+    @staticmethod
+    def default(obj):
         from streamcat.core import Datum
         from streamcat.store import StoreModel
         from streamcat.store import FlowData
@@ -18,5 +19,4 @@ class SCatJSONEncoder(JSONEncoder):
         elif isinstance(obj, (StoreModel, Lock, Datum, FlowData, User, Role, ProjectFolder.Member, Role.Member)):
             return obj.to_json()
         else:
-            # 上記以外のクラスはデフォルトのデコード処理とする
-            return JSONEncoder.default(self, obj)
+            raise TypeError(f'{type(obj).__name__} is not JSON serializable')
