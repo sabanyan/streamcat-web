aea4bac6d3f87ec78a28ccce899a61dde3f4a463
hiroshi80 <it.gumbler@gmail.com>
2023-04-29 15:25:58 +0900

    Fixed error form werkzeug

--- setup.py
+++ setup.py
@@ -11,9 +11,6 @@ setup(
         # テストスクリプトで用いているFlaskのtemplate_renderedが使用する
         'blinker',
         'Flask',
-        # watchdog>2.3.0ではFlaskのデバッグ実行時に不具合がある
-        # https://github.com/pallets/werkzeug/issues/2603
-        'watchdog==2.2.1',
         # 'flask_mail',
         'oauthlib',
         'PyJWT',
