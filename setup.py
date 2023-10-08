923a72e4946f71cbe94b573bbef2a01e314364cb
hiroshi80 <it.gumbler@gmail.com>
2023-10-08 11:21:14 +0900

    Updated backend modules

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ setup(
     include_package_data=True,
     install_requires=[
         # テストスクリプトで用いているFlaskのtemplate_renderedが使用する
-        'blinker==1.6.2',
+        'blinker==1.6.3',
         'Flask==2.3.2',
         # 'flask_mail',
         'oauthlib==3.2.2',
-        'PyJWT==2.6.0',
+        'PyJWT==2.8.0',
     ],
 )
