34c99707a97347fe230f7c25cb52b66010a64511
hiroshi80 <it.gumbler@gmail.com>
2023-10-30 18:18:19 +0900

    Moved range selection and movement funcs to Nodes component

--- streamcat/web/frontend/js/components/FlowEditorContainer/Nodes/index.tsx
+++ streamcat/web/frontend/js/components/FlowEditorContainer/Nodes/index.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import { AllNodeType, FlowType } from 'Model/Library';
+import React, { useEffect } from 'react';
+import { AllNodeType, Flow, FlowType } from 'Model/Library';
 import { DragType, GraphType, RunnablesType } from 'Types/index';
 import { Node } from 'Shared/SVG';
+import {ZoomUtil} from 'Utils/index';
+import { graphUtil } from "Modules/flowEditor";
+import Constants from 'Constants/index';
 
 type Props = {
     selectedNodes: AllNodeType[];
@@ -17,6 +20,15 @@ type Props = {
     addHistory: () => void;
 };
 
+// useStateを使うと期待通り動作しないので修正
+let coords: { x: number, y: number } | null = null;
+let setCoords = (_coords: { x: number, y: number } | null) => {
+    coords = _coords;
+};
+
+let mouseMoveEvent;
+let mouseUpEvent;
+
 export const Nodes = (props:Props) => {
     const {
         selectedNodes,
@@ -31,28 +43,236 @@ export const Nodes = (props:Props) => {
     const [graph, setGraph] = props.graphState;
     const [dragRange, setDragRange] = props.dragRangeState;
     
+
+    /**
+     * 範囲選択との衝突判定
+     */
+    const selectorIntersect = (node:AllNodeType) => {
+        const { zoom } = props;
+        const operator = {
+            x: node.position.x,
+            y: node.position.y,
+            width: Constants.default.step.width,
+            height: Constants.default.step.height
+        };
+
+        if(dragRange){
+            const { start, end } = dragRange;
+
+            //ref:http://gyabo.sakura.ne.jp/tips/rect.html
+
+            let sx = (start.x <= end.x) ? start.x : end.x;
+            let sy = (start.y <= end.y) ? start.y : end.y;
+            let ex = (end.x >= start.x) ? end.x : start.x;
+            let ey = (end.y >= start.y) ? end.y : start.y;
+
+            sx = ZoomUtil.zoomReverse(sx, zoom);
+            sy = ZoomUtil.zoomReverse(sy, zoom);
+            ex = ZoomUtil.zoomReverse(ex, zoom);
+            ey = ZoomUtil.zoomReverse(ey, zoom);
+
+            /**
+             isIntersect = (
+             ((ex >= operator.x && sx <= operator.x) ||
+             (ex >= operator.x + operator.width && sx <= operator.x + operator.width)) &&
+             ((ey >= operator.y && sy <= operator.y) ||
+             (ey >= operator.y + operator.height && sy <= operator.y + operator.height))
+             )
+             */
+            const isIntersect = (sx <= operator.x + operator.width &&
+                operator.x <= ex &&
+                sy <= operator.y + operator.height &&
+                operator.y <= ey);
+
+            if (isIntersect) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        return isSelected(node);
+    };
+
+
+
+    const isSelected = (node:AllNodeType) => {
+        const { selectedNodes } = props;
+        let selected = false;
+        selectedNodes.map(selectedNode => {
+            if (selectedNode.id === node.id) {
+                selected = true;
+            }
+        });
+        return selected;
+    };
+
+
+    useEffect(() => {
+        flow.flow.nodes.forEach(node => {
+            if (selectorIntersect(node)) {
+                if (!isSelected(node)) {
+                    addSelectNode(node);
+                }
+
+            } else {
+                if (isSelected(node)) {
+                    unselectNode(node.id);
+                }
+            }
+        });
+    });
+
+    /**
+     * mouse down Node選択処理
+     * @param e
+     */
+    const onMouseDown = (node:AllNodeType, e: React.MouseEvent<SVGElement>) => {
+        //mousemoveイベントでハンドリング
+        // fix #195
+        if (e.button === 0) onMouseLeftDown(node, e);
+    };
+
+    const onMouseLeftDown = (node:AllNodeType, e: React.MouseEvent<SVGElement>) => {
+        setCoords({
+            x: e.pageX,
+            y: e.pageY
+        });
+        mouseUpEvent = (e: React.MouseEvent<SVGElement>) => handleMouseUp(node, e);
+        mouseMoveEvent = (e: React.MouseEvent<SVGElement>) => handleMouseMove(node, e);
+        document.addEventListener("mousemove", mouseMoveEvent, { passive: true });
+        document.addEventListener("mouseup", mouseUpEvent, { passive: true });
+    };
+
+    /**
+     * mouse up
+     * @param e
+     */
+    const handleMouseUp = (node:AllNodeType, e: React.MouseEvent<SVGElement>) => {
+        setCoords(null);
+
+        const { addSelectNode, unselectNode, selectNodes, addHistory } = props;
+        const [flow, setFlow] = props.flowState;
+
+        //選択イベントの呼び出し
+        if (e.shiftKey) {
+            if (!isSelected(node)) {
+                addSelectNode(node);
+            } else {
+                unselectNode(node.id);
+            }
+        } else {
+            //一度選択状態をクリアする（#71）
+            selectNodes([]);
+            selectNodes([node]);
+        }
+
+        document.removeEventListener("mousemove", mouseMoveEvent);
+        document.removeEventListener("mouseup", mouseUpEvent);
+
+        // flowを更新する
+        setFlow({...flow});
+        // Undoスタックに履歴を追加する
+        addHistory();
+    };
+
+    /**
+     * mouse move Nodeのドラッグ処理
+     * @param e
+     */
+    const handleMouseMove = (node:AllNodeType, e: React.MouseEvent<SVGElement>) => {
+        const { selectedNodes, nodeReadOnly } = props;
+        if (nodeReadOnly) return; // 読み取り専用の場合は移動不可
+
+        if (selectedNodes.length > 1) {
+            // 複数のNodeを一括して移動させる
+            onMoveNodes(node, e);
+        } else {
+            // 単一のNodeを移動させる
+            onMoveNode(node, e);
+        }
+        //一時保存された位置を更新
+        setCoords({
+            x: e.pageX,
+            y: e.pageY
+        });
+    };
+
+    const calcNewPosition = (node:AllNodeType, e: React.MouseEvent<SVGElement>): { new_x: number, new_y: number } => {
+        const { zoom } = props;
+        let coords_x = e.pageX;
+        let coords_y = e.pageY;
+
+        if (coords) {
+            coords_x = coords.x;
+            coords_y = coords.y;
+        }
+
+        //移動量から現在位置を割り出す
+        const xDiff = coords_x - e.pageX;
+        const yDiff = coords_y - e.pageY;
+        const new_x = node.position.x - ZoomUtil.zoomReverse(xDiff, zoom);
+        const new_y = node.position.y - ZoomUtil.zoomReverse(yDiff, zoom);
+        return { new_x: new_x, new_y: new_y };
+    };
+
+    const moveNodes = (flowData:Flow, node:AllNodeType, x: number, y: number, selectedNodes:AllNodeType[]) => {
+        const [graph, setGraph] = props.graphState;
+        const { zoom } = props;
+
+        // 移動距離を算出する
+        const dx = (node.position.x - x);
+        const dy = (node.position.y - y);
+
+        // 選択中の全てのNodeを移動する
+        // 複数のNodeを一括で移動させる場合は、そのNodeの中に自Nodeが含まれていること
+        flowData.nodes.filter(node => selectedNodes.some(selectedNode => selectedNode.id===node.id)).forEach(node => {
+            // Nodeの位置を変更する
+            node.position.x = node.position.x - dx;
+            node.position.y = node.position.y - dy;
+        });
+
+        // Canvasへ反映させる
+        setGraph(graphUtil.getGraph(flowData.nodes, zoom));
+    };
+
+    const onMoveNode = (node:AllNodeType, e: React.MouseEvent<SVGElement>) => {
+        const { new_x, new_y } = calcNewPosition(node, e);
+        moveNodes(flow.flow, node, new_x, new_y, [node]);
+    };
+
+    const onMoveNodes = (node:AllNodeType, e: React.MouseEvent<SVGElement>) => {
+        if (selectedNodes.some(selectedNode => selectedNode.id===node.id)) {
+            const { new_x, new_y } = calcNewPosition(node, e);
+            moveNodes(flow.flow, node, new_x, new_y, selectedNodes);
+        }
+    };
+
+
     return flow.flow.nodes.map(node => {
-        let selected = (node.id === selectedNodes[0]?.id);
         return <Node
             key={node.id}
             node={node}
-            position={node.position}
-            selected={selected}
-            invalid={node.invalid}
-            error={node.error}
+            // position={node.position}
+            selected={selectorIntersect(node)}
+            // invalid={node.invalid}
+            // error={node.error}
             runnables={runnables}
-            flowData={flow.flow}
-            flowState={[flow, setFlow]}
-            graphState={[graph, setGraph]}
-            selectedNodes={selectedNodes}
-            zoom={zoom}
-            dragRange={dragRange}
-            addSelectNode={addSelectNode}
-            unselectNode={unselectNode}
-            selectNodes={selectNodes}
+            // flowData={flow.flow}
+            flowIn =  {flow.flow.ports[0].exists(node.id)}
+            flowOut = {flow.flow.ports[1].exists(node.id)}
+            // flowState={[flow, setFlow]}
+            // graphState={[graph, setGraph]}
+            // selectedNodes={selectedNodes}
+            // zoom={zoom}
+            // dragRange={dragRange}
+            // addSelectNode={addSelectNode}
+            // unselectNode={unselectNode}
+            // selectNodes={selectNodes}
             // selectFrame={frame => setSelectedFrame(frame)}
-            addHistory={addHistory}
-            readOnly={nodeReadOnly}
+            // addHistory={addHistory}
+            // readOnly={nodeReadOnly}
+            onMouseDown={onMouseDown}
         />;
     });
 };
--- streamcat/web/frontend/js/components/shared/SVG/Node/index.tsx
+++ streamcat/web/frontend/js/components/shared/SVG/Node/index.tsx
@@ -8,177 +8,35 @@ import { AllNodeType, Flow, FlowType } from "Model/Library";
 import { CommandNodeType, FlowNodeType, FrameNodeType, InlineFlowNodeType, NoteNodeType } from "Model/Node/NodeTypes";
 import { graphUtil } from "Modules/flowEditor";
 
-let mouseMoveEvent;
-let mouseUpEvent;
-
 interface Props {
     node: AllNodeType;
-    position: { x: number, y: number };
+    // position: { x: number, y: number };
     selected: boolean;
-    invalid?: {};
-    error?: {};
+    // invalid?: {};
+    // error?: {};
     runnables: RunnablesType;
-    flowData: Flow;
-    flowState: [FlowType, (value:React.SetStateAction<FlowType>)=>void];
-    graphState: [GraphType, (value:React.SetStateAction<GraphType>)=>void];
-    selectedNodes: AllNodeType[];
-    zoom: number;
-    dragRange: DragType | null;
-    addSelectNode: (selectedNode: AllNodeType) => void;
-    unselectNode: (selectedNodeId: string) => void;
-    selectNodes: (selectedNodes: AllNodeType[]) => void;
+    // flowData: Flow;
+    flowIn: boolean;
+    flowOut: boolean;
+    // flowState: [FlowType, (value:React.SetStateAction<FlowType>)=>void];
+    // graphState: [GraphType, (value:React.SetStateAction<GraphType>)=>void];
+    // selectedNodes: AllNodeType[];
+    // zoom: number;
+    // dragRange: DragType | null;
+    // addSelectNode: (selectedNode: AllNodeType) => void;
+    // unselectNode: (selectedNodeId: string) => void;
+    // selectNodes: (selectedNodes: AllNodeType[]) => void;
     // selectFrame: (frame?:FrameType) => void;
-    addHistory: () => void;
-    readOnly: boolean;
+    // addHistory: () => void;
+    // readOnly: boolean;
+    onMouseDown: (node:AllNodeType, e: React.MouseEvent<SVGElement>) => void;
 }
 
-// useStateを使うと期待通り動作しないので修正
-let coords: { x: number, y: number } | null = null;
-let setCoords = (_coords: { x: number, y: number } | null) => {
-    coords = _coords;
-};
-
 export const Node = (props: Props) => {
-    const { node } = props;
+    const { node, selected } = props;
 
     const [hover, setHover] = useState<boolean>(false);
 
-    const isSelected = () => {
-        const { selectedNodes } = props;
-        let selected = false;
-        selectedNodes.map(selectedNode => {
-            if (selectedNode.id === node.id) {
-                selected = true;
-            }
-        });
-        return selected;
-    };
-
-    /**
-     * mouse down Node選択処理
-     * @param e
-     */
-    const handleMouseDown = (e: React.MouseEvent<SVGElement>) => {
-        //mousemoveイベントでハンドリング
-        // fix #195
-        if (e.button === 0) onMouseLeftDown(e);
-    };
-
-    const onMouseLeftDown = (e: React.MouseEvent<SVGElement>) => {
-        setCoords({
-            x: e.pageX,
-            y: e.pageY
-        });
-        mouseUpEvent = (e: React.MouseEvent<SVGElement>) => handleMouseUp(e);
-        mouseMoveEvent = (e: React.MouseEvent<SVGElement>) => handleMouseMove(e);
-        document.addEventListener("mousemove", mouseMoveEvent, { passive: true });
-        document.addEventListener("mouseup", mouseUpEvent, { passive: true });
-    };
-
-    /**
-     * mouse up
-     * @param e
-     */
-    const handleMouseUp = (e: React.MouseEvent<SVGElement>) => {
-        setCoords(null);
-
-        const { addSelectNode, unselectNode, selectNodes, addHistory } = props;
-        const [flow, setFlow] = props.flowState;
-
-        //選択イベントの呼び出し
-        if (e.shiftKey) {
-            if (!isSelected()) {
-                addSelectNode(node);
-            } else {
-                unselectNode(node.id);
-            }
-        } else {
-            //一度選択状態をクリアする（#71）
-            selectNodes([]);
-            selectNodes([node]);
-        }
-
-        document.removeEventListener("mousemove", mouseMoveEvent);
-        document.removeEventListener("mouseup", mouseUpEvent);
-
-        // flowを更新する
-        setFlow({...flow});
-        // Undoスタックに履歴を追加する
-        addHistory();
-    };
-
-    /**
-     * mouse move Nodeのドラッグ処理
-     * @param e
-     */
-    const handleMouseMove = (e: React.MouseEvent<SVGElement>) => {
-        const { selectedNodes, readOnly } = props;
-        if (readOnly) return; // 読み取り専用の場合は移動不可
-
-        if (selectedNodes.length > 1) {
-            // 複数のNodeを一括して移動させる
-            onMoveNodes(e);
-        } else {
-            // 単一のNodeを移動させる
-            onMoveNode(e);
-        }
-        //一時保存された位置を更新
-        setCoords({
-            x: e.pageX,
-            y: e.pageY
-        });
-    };
-
-    const calcNewPosition = (e: React.MouseEvent<SVGElement>): { new_x: number, new_y: number } => {
-        const { zoom, position } = props;
-        let coords_x = e.pageX;
-        let coords_y = e.pageY;
-
-        if (coords) {
-            coords_x = coords.x;
-            coords_y = coords.y;
-        }
-
-        //移動量から現在位置を割り出す
-        const xDiff = coords_x - e.pageX;
-        const yDiff = coords_y - e.pageY;
-        const new_x = position.x - ZoomUtil.zoomReverse(xDiff, zoom);
-        const new_y = position.y - ZoomUtil.zoomReverse(yDiff, zoom);
-        return { new_x: new_x, new_y: new_y };
-    };
-
-    const moveNodes = (flowData:Flow, x: number, y: number, selectedNodes:AllNodeType[]) => {
-        const [graph, setGraph] = props.graphState;
-        const { zoom } = props;
-
-        // 移動距離を算出する
-        const dx = (node.position.x - x);
-        const dy = (node.position.y - y);
-
-        // 選択中の全てのNodeを移動する
-        // 複数のNodeを一括で移動させる場合は、そのNodeの中に自Nodeが含まれていること
-        flowData.nodes.filter(node => selectedNodes.some(selectedNode => selectedNode.id===node.id)).forEach(node => {
-            // Nodeの位置を変更する
-            node.position.x = node.position.x - dx;
-            node.position.y = node.position.y - dy;
-        });
-
-        // Canvasへ反映させる
-        setGraph(graphUtil.getGraph(flowData.nodes, zoom));
-    };
-
-    const onMoveNode = (e: React.MouseEvent<SVGElement>) => {
-        const { new_x, new_y } = calcNewPosition(e);
-        moveNodes(flowData, new_x, new_y, [node]);
-    };
-
-    const onMoveNodes = (e: React.MouseEvent<SVGElement>) => {
-        const { selectedNodes } = props;
-        if (selectedNodes.some(selectedNode => selectedNode.id===node.id)) {
-            const { new_x, new_y } = calcNewPosition(e);
-            moveNodes(flowData, new_x, new_y, selectedNodes);
-        }
-    };
 
     /**
      * mouse over ホバー処理
@@ -197,57 +55,6 @@ export const Node = (props: Props) => {
     const handleMouseLeave = () => {
         //SVGの影をクリア
         setHover(false);
-    };
-
-
-    /**
-     * 範囲選択との衝突判定
-     */
-    const selectorIntersect = () => {
-        const { zoom, position, dragRange } = props;
-        const operator = {
-            x: position.x,
-            y: position.y,
-            width: Constants.default.step.width,
-            height: Constants.default.step.height
-        };
-
-        if(dragRange){
-            const { start, end } = dragRange;
-
-            //ref:http://gyabo.sakura.ne.jp/tips/rect.html
-
-            let sx = (start.x <= end.x) ? start.x : end.x;
-            let sy = (start.y <= end.y) ? start.y : end.y;
-            let ex = (end.x >= start.x) ? end.x : start.x;
-            let ey = (end.y >= start.y) ? end.y : start.y;
-
-            sx = ZoomUtil.zoomReverse(sx, zoom);
-            sy = ZoomUtil.zoomReverse(sy, zoom);
-            ex = ZoomUtil.zoomReverse(ex, zoom);
-            ey = ZoomUtil.zoomReverse(ey, zoom);
-
-            /**
-             isIntersect = (
-             ((ex >= operator.x && sx <= operator.x) ||
-             (ex >= operator.x + operator.width && sx <= operator.x + operator.width)) &&
-             ((ey >= operator.y && sy <= operator.y) ||
-             (ey >= operator.y + operator.height && sy <= operator.y + operator.height))
-             )
-             */
-            const isIntersect = (sx <= operator.x + operator.width &&
-                operator.x <= ex &&
-                sy <= operator.y + operator.height &&
-                operator.y <= ey);
-
-            if (isIntersect) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        return isSelected();
     };
 
 
@@ -273,23 +80,23 @@ export const Node = (props: Props) => {
         const filter = "url(#default-shadow)";
         return filter;
     };
-    useEffect(() => {
-        const { addSelectNode, unselectNode } = props;
-        // componentDidUpdate
-        if (selectorIntersect()) {
-            if (!isSelected()) {
-                addSelectNode(node);
-            }
+    // useEffect(() => {
+    //     const { selected, addSelectNode, unselectNode } = props;
+    //     // componentDidUpdate
+    //     if (selected) {
+    //         if (!selected) {
+    //             addSelectNode(node);
+    //         }
 
-        } else {
-            if (isSelected()) {
-                unselectNode(node.id);
-            }
-        }
-    });
+    //     } else {
+    //         if (selected) {
+    //             unselectNode(node.id);
+    //         }
+    //     }
+    // });
 
-    const { position, runnables, flowData, invalid, error } = props;
-    const { x, y } = position;
+    const { flowIn, flowOut, runnables, onMouseDown } = props;
+    const { x, y } = node.position;
     let icon: JSX.Element | null;
 
     /**
@@ -298,10 +105,10 @@ export const Node = (props: Props) => {
 
     const filter = getFilter();
 
-    const selected = selectorIntersect();
+    // const selected = selectorIntersect();
 
-    const flowIn = flowData.ports[0].exists(node.id);
-    const flowOut = flowData.ports[1].exists(node.id);
+    // const flowIn = flowData.ports[0].exists(node.id);
+    // const flowOut = flowData.ports[1].exists(node.id);
 
     let nodeLabel = node.label;
 
@@ -363,8 +170,8 @@ export const Node = (props: Props) => {
     }
 
 
-    const invalid_icon = (invalid && Object.keys(invalid).length) ? <ErrorIcon /> : null;
-    const error_icon = (error && Object.keys(error).length) ? <ErrorIcon /> : null;
+    const invalid_icon = (node.invalid && Object.keys(node.invalid).length) ? <ErrorIcon /> : null;
+    const error_icon = (node.error && Object.keys(node.error).length) ? <ErrorIcon /> : null;
     const label_text = (!!nodeLabel) ? 
                         <g className={style.labelContainer}>
                             <foreignObject {...NodeTextStyle} transform={"translate(" + (-1 * NodeTextStyle.width) + ",0)"}>
@@ -387,7 +194,8 @@ export const Node = (props: Props) => {
 
     return (
         <g className={style.operator} transform={"translate(" + x + "," + y + ")"}>
-            <g className={style.iconContainer} onMouseDown={(e: React.MouseEvent<SVGElement>) => handleMouseDown(e)}
+            <g className={style.iconContainer}
+                onMouseDown={e => onMouseDown(node, e)}
                 onMouseOver={() => handleMouseOver()}
                 onMouseLeave={() => handleMouseLeave()}>
                 {icon}
