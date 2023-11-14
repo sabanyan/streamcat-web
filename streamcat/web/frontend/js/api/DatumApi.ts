4422db5fc552b660a5d55e47df55e95796c644e9
hiroshi80 <it.gumbler@gmail.com>
2023-11-14 17:03:25 +0900

    Moved some API to SystemApi

--- streamcat/web/frontend/js/api/DatumApi.ts
+++ streamcat/web/frontend/js/api/DatumApi.ts
@@ -21,10 +21,6 @@ import {
     FlowCommands
 } from 'Model/Library';
 import {
-    ConnectivityType,
-    NavigationType
-} from 'Model/Navigation/NavigationModel';
-import {
     getBase as get,
     postBase,
     putBase,
@@ -513,60 +509,6 @@ export const DatumApi = {
      */
     findVCommands: () => {
         return get<VCommand[]>('/api/v0/vcommands');
-    },
-
-    /**
-     * GET /navigationを発行してNavigationを取得する
-     * @throws {ErrorResponse}
-     */
-    findNavigation: () => {
-        return get<NavigationType>('/api/v0/navigation');
-    },
-
-    /**
-     * GET /connectables/remote-foldersを発行して
-     * RemoteFolderへの接続を確認する
-     */
-    checkRemoteFolderConnection: (
-        protocol: 'smb',
-        hostname: string,
-        domain: string,
-        directory: string,
-        userId: string,
-        password: string
-    ) => {
-        const params = {
-            protocol: protocol,
-            hostname: hostname,
-            domain: domain,
-            directory: directory,
-            userId: userId,
-            password: password
-        };
-        return get<ConnectivityType>('/api/v0/connections/remote-folders', params);
-    },
-
-    /**
-     * GET /connectables/databasesを発行して
-     * Databaseへの接続を確認する
-     */
-    checkDatabaseConnection: (
-        dbms: 'postgresql'|'oracle',
-        hostname: string,
-        port: number,
-        database: string,
-        userId: string,
-        password: string
-    ) => {
-        const params = {
-            dbms: dbms,
-            hostname: hostname,
-            port: port,
-            database: database,
-            userId: userId,
-            password: password
-        };
-        return get<ConnectivityType>('/api/v0/connections/databases', params);
     },
 
     /**
