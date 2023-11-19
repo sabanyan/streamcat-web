11b9ef72568c485d5e41862a8730c447aeacf3a8
hiroshi80 <it.gumbler@gmail.com>
2023-11-19 20:34:28 +0900

    Removed libdefs.js

--- streamcat/web/frontend/lib/libdefs.js
+++
@@ -1,5 +0,0 @@
-//@flow
-//ref:https://wp-kyoto.net/add-flowtype-in-imported-react-component
-
-declare module 'eventemitter3' { declare module.exports: any; }
-declare module 'classnames' { declare module.exports: any; }
