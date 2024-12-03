dd3fb0d1bf2446f3c7cd36f3dcf6004e4f428406
hiroshi80 <it.gumbler@gmail.com>
2024-12-03 12:43:09 +0900

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ setup(
         'python-multipart==0.0.19',
         'jinja2==3.1.4',
         # FastAPIでDatumをJSONに変換する時に使用する
-        'orjson==3.10.12'
+        'orjson==3.10.12',
         # FastAPIでデコレータ内でRequestオブジェクトを取得できるようにする
-        'fastapi-decorators==1.0.6'
+        'fastapi-decorators==1.0.6',
         # FastAPIのTestClientが使用する
-        'httpx==0.28.0'
+        'httpx==0.28.0',
         # 'Flask==2.3.2',
         # 'flask_mail',
         'oauthlib==3.2.2',
