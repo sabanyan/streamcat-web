a77847480e83dd08efbd69ef36dfa501d6962128
hiroshi80 <it.gumbler@gmail.com>
2025-09-15 11:24:17 +0900

    Updated backend modules

--- setup.py
+++ setup.py
@@ -10,21 +10,21 @@ setup(
     install_requires=[
         # テストスクリプトで用いているFlaskのtemplate_renderedが使用する
         # 'blinker==1.9.0',
-        'fastapi==0.115.8',
+        'fastapi==0.116.1',
         # FastAPIでMultipartリクエストを扱う場合に使用する
         'python-multipart==0.0.20',
-        'jinja2==3.1.5',
+        'jinja2==3.1.6',
         # FastAPIでDatumをJSONに変換する時に使用する
-        'orjson==3.10.15',
+        'orjson==3.11.3',
         # FastAPIでデコレータ内でRequestオブジェクトを取得できるようにする
-        'fastapi-decorators==1.0.6',
+        'fastapi-decorators==1.0.19',
         # FastAPIのTestClientが使用する
         'httpx==0.28.1',
         # 'Flask==3.1.0',
         # 'flask_mail',
-        'oauthlib==3.2.2',
+        'oauthlib==3.3.1',
         'PyJWT==2.10.1',
         # "standard"を指定して"--reload"指定によるCPUの高負荷を下げる
-        'uvicorn[standard]==0.34.0'
+        'uvicorn[standard]==0.35.0'
     ],
 )
